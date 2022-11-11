69e6114278757162e0e0e63f456ec1f164b24e2b
dungdung7301 <81977385+dungdung7301@users.noreply.github.com>
2022-11-11 13:35:15 +0700

    title

---
+++ pages/userList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Auth from "layouts/Auth.js";
+import Title from "public/shared/Title";
+
+export default function userList() {
+  return <h2>Danh sách người chơi</h2>;
+}
+userList.layout = Auth;
