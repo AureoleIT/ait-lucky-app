33092229857397488bc80e7ee8d90eb7ba2952ab
phongptt <pttp1512@gmail.com>
2022-11-11 10:32:18 +0700

    Upadate spacing

--- public/shared/TickBox.js
+++ public/shared/TickBox.js
@@ -4,7 +4,7 @@ export default function TickBox({content, htmlFor}) {
     return (
         <>
             <input type="checkbox" className="border-[#004599] border" />
-            <label htmlFor={htmlFor}>
+            <label htmlFor={htmlFor} className="relative left-[-10px]">
                 <p className="font-bold text-[#004599]">{content}</p>
             </label>
         </>
