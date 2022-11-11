30dc3051b20ce413b477d1ba78872068690ebc74
phongptt <pttp1512@gmail.com>
2022-11-11 10:32:36 +0700

    Update GradientLine component

--- public/shared/GradientLine.js
+++ public/shared/GradientLine.js
@@ -2,10 +2,14 @@ import React from "react";
 
 export default function GradientLine({color1, color2, content}) {
     const gradientCSS = `bg-gradient-to-r from-[${color1}] to-[${color2}]`
+    let bgColor = "bg-white"
+    if(content === "") {
+        bgColor = ""
+    }
 
     return (
         <div className={`w-full h-[2px] ${gradientCSS} relative flex justify-center`}>
-            <p className="absolute top-[-50%] transform translate-y-[-50%] bg-white px-[10px] text-[#003B93] font-semibold">{content}</p>
+            <p className={`absolute top-[-50%] transform translate-y-[-50%] px-[10px] text-[#003B93] font-semibold ${bgColor}`}>{content}</p>
         </div>
     )
 }
