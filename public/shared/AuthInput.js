c938911ed1f51fe362882333b784e50acb2a4db4
ytn <a11718trannhuy@gmail.com>
2022-11-11 16:29:00 +0700

    Component added

--- public/shared/AuthInput.js
+++ public/shared/AuthInput.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AuthInput({content, type}) {
+
+    const [text, setText] = useState("")
+
     const contentCSS = {
         background: "-webkit-linear-gradient(45deg, #003B93, #00F0FF)",
         "-webkit-background-clip": "text",
@@ -10,7 +13,7 @@ export default function AuthInput({content, type}) {
     return (
         <div className="bg-gradient-to-r from-[#003B93] to-[#00F0FF] p-[2px] rounded-[10px] w-full h-[60px] py-[2px] outline-none relative">
             <div className="h-full">
-                <input type={type} className="h-full w-full rounded-lg px-2 outline-none border-none" />
+                <input type={type} value={text} className="h-full w-full rounded-lg px-2 outline-none border-none" onChange={(e) => setText(e.target.value)}/>
                 <div className="bg-white absolute w-full top-0">
                     <label htmlFor="" className="absolute px-[10px] mx-[15px] bg-white transform translate-y-[-50%] left-0">
                         <p style={contentCSS} className="font-bold text-[16px]">{content}</p>
--- public/shared/BgBlueButton.js
+++ public/shared/BgBlueButton.js
@@ -1,14 +1,28 @@
 import React from "react";
+import Link from "next/link"
 
-export default function BgBlueButton({content, onClick}) {
+export default function BgBlueButton({content, onClick, islink, href }) {
     return (
         <>
-            <button className="w-full h-[50px] bg-gradient-to-r from-[#003B93] to-[#00F0FF] rounded-[50px]"
-            onClick={onClick}>
-                <div className="font-[900] text-[24px] text-white">
-                    {content}
-                </div>
-            </button>
+            {
+                islink ? (
+                    <Link href={href}>
+                        <button className="w-full h-[50px] bg-gradient-to-r from-[#003B93] to-[#00F0FF] rounded-[50px]"
+                        onClick={onClick}>
+                            <div className="font-[900] text-[24px] text-white">
+                                {content}
+                            </div>
+                        </button>
+                    </Link>
+                ) : (
+                    <button className="w-full h-[50px] bg-gradient-to-r from-[#003B93] to-[#00F0FF] rounded-[50px]"
+                    onClick={onClick}>
+                        <div className="font-[900] text-[24px] text-white">
+                            {content}
+                        </div>
+                    </button>
+                )
+            }
         </>
     )
 }
