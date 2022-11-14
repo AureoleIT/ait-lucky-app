eb0eec41d4e826cf25318de169e30b75dffe87c4
ytn <a11718trannhuy@gmail.com>
2022-11-14 10:58:39 +0700

    event reward register update ui

--- pages/_registergift.js
+++ pages/_registergift.js
@@ -1,12 +1,11 @@
-import React, { createContext, useState } from "react";
-import Link from 'next/link'
+import React, { useState } from "react";
 
 import { MoreGift } from "components/JoinEvent/MoreGift";
 import AuthInput from "public/shared/AuthInput";
 import TextArea from "public/shared/TextArea";
 import BgBlueButton from "public/shared/BgBlueButton";
 import BgRedButton from "public/shared/BgRedButton";
-import ButtonWithIcon from "public/shared/ButtonWithIcon";
+import ButtonAndIcon from "public/shared/ButtonAndIcon";
 
 
 function EventRewardRegister () {
@@ -57,30 +56,36 @@ function EventRewardRegister () {
     return (
             <div className="flex flex-col items-center justify-evenly h-screen w-screen">
                 <div className="flex flex-col items-center justify-center h-full max-w-[300px]">
-                    <h1 className="uppercase text-4xl py-2 font-bold" style={contentCSS}>đăng ký</h1>
-                    <h1 className="uppercase text-xl py-2 mb-2 font-bold"  style={contentCSS}>thông tin giải thưởng</h1>
+                    <h1 className="uppercase text-4xl py-2 font-bold text-[#004599]">đăng ký</h1>
+                    <h1 className="uppercase text-xl py-2 mb-2 font-bold text-[#004599]">thông tin giải thưởng</h1>
                     {/* gift name */}
 
                     <AuthInput content={"Tên giải thưởng"} type={"text"} />
 
                     <div className="flex justify-evenly w-full my-2 lg:justify-center">
                         <label style={contentCSS}>Số lượng</label>
-                        <div className="flex justify-between items-center text-lg">
+                        <div className="flex justify-between items-center text-lg" style={contentCSS}>
                             <i className="fas fa-minus px-2 flex justify-center items-center cursor-pointer" style={icon} onClick={() => setGiftCount(giftCount - 1)}></i>
-                            {giftCount}
+                            <label>{giftCount}</label>
                             <i className="fas fa-plus px-2 flex justify-center items-center cursor-pointer" style={icon} onClick={() => setGiftCount(giftCount + 1)}></i>
                         </div>
                     </div>
                     {/* gift detail */}
-                    <div className="relative my-3 w-[90%] lg:w-6/12" style={border}>
-                        <label className="absolute top-0" style={label}><p style={contentCSS}>Phần quà</p></label>
-                    </div>
-                    <div className="overflow-x-hidden overflow-y-auto flex flex-col justify-center items-center max-h-[250px] w-full max-w-[300px]">
-                        <div className="flex py-3 justify-between items-center relative w-full">
-                            <TextArea  content={"Mô tả phần quà"} row={"3"}/>
-                            <i className="fas fa-trash text-red-600 cursor-pointer px-[1rem]" onClick={handleDeleteGift}></i>
+                        {/* line */}
+                    <div class="w-[90%] max-w-sm">
+                        <div class="w-full h-[2px] my-4 bg-gradient-to-r from-[#003B93] to-[#00F0FF] relative flex">
+                            <p class="absolute top-[-50%] transform translate-y-[-50%] mx-[10px] px-[10px] text-[#003B93] font-semibold bg-white">Phần quà</p>
                         </div>
-                        <ButtonWithIcon content={"Thêm hình ảnh"} classIcon={"fas fa-image"}/>
+                    </div>
+
+                    <div className="overflow-x-hidden overflow-y-auto flex flex-col h-[190px] w-full max-w-[300px]">
+                        <div className="w-full h-full">
+                            <div className="flex py-3 justify-between items-center relative w-full h-full">
+                                <TextArea  content={"Mô tả phần quà"} row={"3"}/>
+                                <i className="fas fa-trash text-red-600 cursor-pointer px-[1rem]" onClick={handleDeleteGift}></i>
+                            </div>
+                        </div>
+                        <ButtonAndIcon content={"Thêm hình ảnh"} classIcon={"fas fa-image"} colorHex={"#40BEE5"}/>
                         {
                             giftList.map((item) =>
                             {
@@ -89,16 +94,28 @@ function EventRewardRegister () {
                         }
                     </div>
                     {/* gift event */}
-                    <hr className="my-2 w-6/12" style={hrStyle}/>
+                        {/* line */}
+                    <div class="w-[90%] max-w-sm">
+                        <div class="w-full h-[2px] my-4 bg-gradient-to-r from-[#003B93] to-[#00F0FF] relative flex">
+                            <p class="absolute top-[-50%] transform translate-y-[-50%] mx-[10px] px-[10px] text-[#003B93] font-semibold bg-white"></p>
+                        </div>
+                    </div>
+
                     <div className="flex justify-center items-center my-2 w-[300px] cursor-pointer" onClick={handleAddGift}>
-                        <BgBlueButton content={"Thêm phần quà"}/>
+                        <ButtonAndIcon content={"Thêm phần quà"} colorHex={"#40BEE5"}/>
                     </div>
                     <div className="flex justify-center items-center my-2 w-[300px] text-white cursor-pointer">
-                        <BgRedButton content={"Hủy giải thưởng"}/>
+                        <ButtonAndIcon content={"Hủy giải thưởng"} colorHex={"#FF6262"}/>
                     </div>
-                    <hr className="my-2 w-6/12" style={hrStyle}/>
+                        {/* line */}
+                    <div class="w-[90%] max-w-sm">
+                        <div class="w-full h-[2px] my-4 bg-gradient-to-r from-[#003B93] to-[#00F0FF] relative flex">
+                            <p class="absolute top-[-50%] transform translate-y-[-50%] mx-[10px] px-[10px] text-[#003B93] font-semibold bg-white"></p>
+                        </div>
+                    </div>
+
                     <div className="flex justify-center items-center my-2 w-[300px] cursor-pointer">
-                        <BgBlueButton content={"Thêm giải thưởng"}/>
+                        <ButtonAndIcon content={"Thêm giải thưởng"} colorHex={"#40BEE5"}/>
                     </div>
                 </div>
             
