a3e9fe9631f6c4959ff42b8c0155eaf6253cecf6
Nguyen Anh Tuan <77793919+ng-anhhtuann@users.noreply.github.com>
2022-11-14 08:58:15 +0700

    Revert "Features/tuan/signup landing screen"

--- public/colors.js
+++
@@ -1,6 +0,0 @@
-export const BG = "bg-gradient-to-tr from-[#C8EFF1] via-[#B3D2E9] to-[#B9E4A7]";
-export const TEXT = '#004599';
-export const LEFT_GRADIENT = "bg-gradient-to-r from-[#003B93] to-[#004293";
-export const RIGHT_GRADIENT = "bg-gradient-to-r from-[#004593] to-[#00FfFF]";
-export const BUTTON_GRADIENT = "bg-gradient-to-r from-[#003B93] to-[#00F0FF]";
-export const BG_WHITE = "bg-white";
--- public/shared/Title.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Title({title}) {
-    return (
-        <h1 className=" font-[900] uppercase text-[#004599] text-4xl text-center mb-6">{title}</h1>
-    )
-}
