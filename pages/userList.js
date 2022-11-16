9639b708428374542b600d0e90480c8e84c3e0e3
dungdung7301 <81977385+dungdung7301@users.noreply.github.com>
2022-11-16 08:27:18 +0700

    Resolve conflicts

--- pages/userList.js
+++ pages/userList.js
@@ -1,8 +1,39 @@
-import React from "react";
 import Auth from "layouts/Auth.js";
-import Title from "public/shared/Title";
+import React, { useState } from "react";
+import { Link } from "next/link";
+import { useForm } from "react-hook-form";
 
+import Logotic from "public/shared/Logotic";
+import AuthInput from "public/shared/AuthInput";
+import TickBox from "public/shared/TickBox";
+import BgBlueButton from "public/shared/BgBlueButton";
+import BgWhiteButton from "public/shared/BgWhiteButton";
+import GradientLine from "public/shared/GradientLine";
+import Title from "public/shared/Title";
+import AuthFooter from "public/shared/AuthFooter";
 export default function userList() {
-  return <h2>Danh sách người chơi</h2>;
+  return (
+    /*<section className="h-screen max-w-[360px] px-5 py-5 mx-auto flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <Title title="DANH SÁCH NGƯỜI CHƠI"></Title>
+      </div>
+    </section>
+    */
+    <section className="h-screen max-w-[360px] px-5 py-5 mx-auto flex flex-col justify-center ">
+      <div className="flex flex-col justify-center items-center w-full">
+        <h1 className="text-[#004599] font-black text-xl">
+          DANH SÁCH NGƯỜI CHƠI
+        </h1>
+        <GradientLine
+          color1="#003B93"
+          color2="#00F0FF"
+          content=""
+        ></GradientLine>
+      </div>
+      <div className="mt-[17px]">
+        <p className="text-[#004599] font-extrabold">Trực tuyến</p>
+      </div>
+    </section>
+  );
 }
 userList.layout = Auth;
