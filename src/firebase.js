bdc8879a53d5213b8c2ab09737f549a69d670728
Nguyen Anh Tuan <nguyenanhtuan1232@gmail.com>
2022-11-17 00:01:05 +0700

    Continue with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
-import { initializeApp } from "firebase/app";
-import firebase from "firebase/compat/app"
+import { initializeApp, firebase } from "firebase/app";
+// import firebase from "firebase/compat/app"
 import "firebase/compat/auth";
-import { getFirestore } from "firebase/firestore"
+import "firebase/auth";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,16 +11,17 @@ import { getFirestore } from "firebase/firestore"
 const firebaseConfig = {
   apiKey: "AIzaSyA8-_ktKoGnG87bJksBu06_o489Xj8V6tU",
   authDomain: "ait-lucky-app.firebaseapp.com",
-  databaseURL: "https://ait-lucky-app-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://ait-lucky-app-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "ait-lucky-app",
   storageBucket: "ait-lucky-app.appspot.com",
   messagingSenderId: "722133037936",
   appId: "1:722133037936:web:501e76a24dca2af4fd55b5",
-  measurementId: "G-S0QRME7VTZ"
+  measurementId: "G-S0QRME7VTZ",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = getDatabase(app);
 const auth = app.auth();
-export {app, db, auth};
+export { app, db, auth };
