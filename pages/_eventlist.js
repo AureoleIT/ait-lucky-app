d2d66def30ee980fd1b0828d53fdaa5d78af65c4
thuyntc <camthuy0212@gmail.com>
2022-11-16 17:26:07 +0700

    Update EventButton

--- pages/_eventlist.js
+++ pages/_eventlist.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import AuthInput from "public/shared/AuthInput";
-import EventListButton from "public/shared/EventListButton";
+import EventBotton from "public/shared/EventButton";
 
 export default function EventList() {
-  const [eventID, setEventID] = useState("EV20221011");
   return (
     <>
       <section className="h-screen px-5 py-5 mx-auto flex justify-center items-center">
@@ -16,25 +15,33 @@ export default function EventList() {
               <AuthInput content={"Tên sự kiện"} type={"text"} />
             </div>
             <div className="flex flex-col gap-y-[7px] w-full font-[Nunito Sans] font-bold">
-              <EventListButton
+              <EventBotton
                 name={"tiệc cuối năm"}
-                time={"EV20221011"}
+                id={"EV20221011"}
                 numberOfPeople={"20"}
+                islink={true}
+                href={"_countdowncheckin"}
               />
-              <EventListButton
+              <EventBotton
                 name={"Tiệc năm mới"}
-                time={"EV20221011"}
+                id={"EV20221011"}
                 numberOfPeople={"20"}
+                islink={true}
+                href={"_eventdetail"}
               />
-              <EventListButton
+              <EventBotton
                 name={"TIỆC THÀNH LẬP..."}
-                time={"EV20221011"}
+                id={"EV20221011"}
                 numberOfPeople={"20"}
+                islink={true}
+                href={"_eventdetail"}
               />
-              <EventListButton
+              <EventBotton
                 name={"TIỆC CUỐI NĂM"}
-                time={"EV20221011"}
+                id={"EV20221011"}
                 numberOfPeople={"20"}
+                islink={true}
+                href={"_eventdetail"}
               />
             </div>
           </div>
