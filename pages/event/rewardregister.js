ceb7d0400b08abc5033e6b9d736fe042081cf8e9
Jin Eu <a11718trannhuy@gmail.com>
2022-11-21 09:31:39 +0700

    edit reward register follow design

--- pages/event/rewardregister.js
+++ pages/event/rewardregister.js
@@ -1,23 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import { MoreGift } from "components/JoinEvent/MoreGift";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router"
 
 import AuthInput from "public/shared/AuthInput";
-import Line from "public/shared/Line";
 import BgBlueButton from "public/shared/BgBlueButton";
+import ImageCustom from "public/shared/ImageCustom";
+import Line from "public/shared/Line";
 import SingleColorButton from "public/shared/SingleColorButton";
 
+
 function EventRewardRegister () {
-    
+ 
+    // router
+    const router = useRouter()
+
     const [giftCount, setGiftCount] = useState(1)
     const [giftName, setGiftName] = useState("")
-    const [giftDetail, setGiftDetail] = useState("")
-    const [giftList, setGiftList] = useState([<MoreGift />])
+    const [imgList, setImgList] = useState([])
+    const [file, setFile] = useState()
 
     const contentCSS = {
         background: "-webkit-linear-gradient(45deg, #003B93, #00F0FF)",
-        "-webkit-background-clip": "text",
-        "-webkit-text-fill-color": "transparent",
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent",
     }
     // icon plus/minus
     const icon = {
@@ -25,124 +30,114 @@ function EventRewardRegister () {
         height:"32px",
         margin:"0 12px",
     }
-    // logic
 
-    // handle register button
-    const handleRegisterButton = () =>
-    {
-        if(giftName !== "" && giftDetail !== "")
-        {
-            alert("Thanh cong !!!")
-        }
-        else 
-        {
-            alert("Vui lòng nhập đủ thông tin !!!")
-        }
+    const iconStyle = {
+        color:"white",
+        fontSize:"24px"
     }
-    // handle add gift
-    const handleAddGift = () =>
+
+    const buttonColor = {
+        background:"#40BEE5"
+    }
+
+    useEffect(() =>
     {
-        // add gift
-        setGiftList(prev => {
-            const newList = [...giftList, <MoreGift />]
-            return newList
+        setImgList(prev => 
+            {
+                let newImgList = []
+                if(file !== undefined)
+                {
+                    newImgList = [...prev, <ImageCustom image={file}/>]
+                }
+                return newImgList
         })
-    }
-    // handle delete gift
-    const handleDeleteGift = (id) =>
-    {
-        const newGiftList = giftList.filter((item,index) => (index !== id))
-        setGiftList(newGiftList)
-        console.log("delete");
-    }
-    // handle delete reward
-    const handleDeleteReward = () =>
-    {
-        
-    }
-    // handle add reward
-    const handleAddReward = () =>
-    {
+    },[file])
 
+    const handleChangeFile = (e) =>
+    {
+        const upload = e.target.files[0]
+        setFile(URL.createObjectURL(upload))
+
+        let toggle = document.getElementById("toggleIMG")
+        toggle.style.display = "flex"
+    }
+    
+    const getImage = (e) =>
+    {
+        document.getElementById("getFile").click()
+    }
+
+    const handleNavigate = () =>
+    {
+        if(giftName !== "")
+        {
+            router.push("/event/eventdetail")
+        }
     }
 
     return (
-            <div className="flex flex-col items-center justify-evenly w-screen h-screen py-2">
-                <div className="flex flex-col items-center h-full w-full">
-                    <h1 className="uppercase text-4xl py-1 font-bold text-[#004599]">đăng ký</h1>
+            <div className="flex flex-col items-center justify-between w-screen h-screen py-2">
+                <div className="flex flex-col items-center justify-center w-full">
                     <h1 className="uppercase text-xl py-2 mb-2 font-bold text-[#004599]">thông tin giải thưởng</h1>
-                    {/* gift name */}
-
-                    <div className="w-[90%] max-w-xl lg:w-4/12">
-                        <AuthInput content={"Tên giải thưởng"} type={"text"} value={giftName} onChange={(e) => setGiftName(e.target.value)} />
+                    <div className="flex w-[90%] lg:w-4/12 max-w-xl">
+                        <div className="flex flex-col justify-evenly mr-4">
+                            <i className="fas fa-angle-up cursor-pointer"></i>
+                            <i className="fas fa-angle-down cursor-pointer"></i>
+                        </div>
+                        <div className="w-full">
+                            <AuthInput content={"Tên giải thưởng"} type={"text"} onChange={(e) => setGiftName(e.target.value)}/>
+                        </div>
                     </div>
 
-                    <div className="flex justify-evenly items-center w-full mt-1 mb-2 lg:justify-center">
+                    <div className="flex justify-evenly items-center w-full lg:w-4/12 mt-1 mb-4 lg:justify-center">
                         <label className="font-bold" style={contentCSS}>Số lượng</label>
                         <div className="flex justify-between items-center text-lg font-bold" style={contentCSS}>
-                            <i className="fas fa-minus px-2 flex justify-center items-center cursor-pointer" style={icon} onClick={() => setGiftCount(giftCount - 1)}></i>
+                            <i className="fas fa-minus px-2 flex justify-center items-center cursor-pointer" style={icon} onClick={() => setGiftCount(Math.max(giftCount - 1, 1))}></i>
                             <label>{giftCount}</label>
                             <i className="fas fa-plus px-2 flex justify-center items-center cursor-pointer" style={icon} onClick={() => setGiftCount(giftCount + 1)}></i>
                         </div>
                     </div>
-                    {/* gift detail */}
-                        {/* line */}
-                    <div className="w-[90%] lg:w-4/12 mb-2 max-w-xl">
-                        <Line content={"Phần quà"}/>
+
+                    <div className="w-[90%] lg:w-4/12 max-w-xl flex mb-2">
+                        <Line content={"Hình ảnh giải thưởng"}/>
                     </div>
 
-                    <div className="flex flex-col w-[90%] max-w-xl my-1 h-[210px] max-h-[210px]">
-                        <div className="h-full w-full flex justify-evenly">
-                            <div className="flex flex-col w-[10%] lg:w-[20%] h-full justify-between items-center text-[24px] leading-[3rem] text-[#003B93]">
-                                <i className="fas fa-angle-up cursor-pointer"></i>
-                                <i className="fas fa-angle-down cursor-pointer"></i>
-                            </div>
-                            <div className="ml-3 w-[90%] lg:w-[80%] h-full overflow-auto">
-                                {giftList.map((item, index) =>
+                    <div className="flex w-[90%] lg:w-4/12 max-w-xl mb-2 overflow-x-auto overflow-y-hidden ">
+                        <div className="w-full mb-2 hidden" id="toggleIMG">
+                            {
+                                imgList.map((item,index) =>
                                 {
                                     return (
-                                        <div className="w-full h-full flex items-center" key={index}>
+                                        <div key={index} className="mx-2 ">
                                             {item}
-                                            <i className="fas fa-trash text-red-600 cursor-pointer px-[1rem]" onClick={() => handleDeleteGift(index)}></i>
                                         </div>
                                     )
-                                })}
-                            </div>
+                                })
+                            }
                         </div>
-
-                    </div>
-                    {/* gift event */}
-                        {/* line */}
-                    <div className="w-[90%] lg:w-4/12 max-w-xl">
-                       <Line />
                     </div>
 
-                    <div className="flex justify-center items-center my-2 w-[90%] lg:w-4/12 max-w-xl cursor-pointer drop-shadow-lg">
-                        <SingleColorButton content={"Thêm phần quà"} colorHex={"#40BEE5"} onClick={handleAddGift} />
-                    </div>
-                    <div className="flex justify-center items-center my-2 w-[90%] lg:w-4/12 max-w-xl text-white cursor-pointer drop-shadow-lg">
-                        <SingleColorButton content={"Hủy giải thưởng"} colorHex={"#FF6262"} onClick={handleDeleteReward}/>
-                    </div>
-                        {/* line */}
                     <div className="w-[90%] lg:w-4/12 max-w-xl">
+                        <button className="flex justify-evenly items-center w-full h-[35px] rounded-[5px]" style={buttonColor} onClick={getImage}>
+                            <div className="font-[900] text-[24px] text-white">Thêm hình ảnh</div>
+                            <i className="fas fa-image" style={iconStyle}></i>
+                        </button>
+                        <input type={"file"} id={"getFile"} onChange={handleChangeFile} style={{display:"none"}}/>
+                    </div>
+
+                    <div className="w-[90%] lg:w-4/12 max-w-xl flex mb-2">
                         <Line />
                     </div>
 
                     <div className="flex justify-center items-center mb-2 mt-1 w-[90%] lg:w-4/12 max-w-xl cursor-pointer drop-shadow-lg">
-                        <SingleColorButton content={"Thêm giải thưởng"} colorHex={"#40BEE5"} onClick={handleAddReward} />
+                        <SingleColorButton content={"Thêm giải thưởng"} colorHex={"#40BEE5"} />
                     </div>
+
                 </div>
 
-                <div className="pb-4 w-11/12 lg:w-4/12 drop-shadow-lg max-w-xl" onClick={handleRegisterButton}>
-                    {
-                        (giftName !== "" && giftDetail !== "") ? (
-                            <BgBlueButton content={"ĐĂNG KÝ SỰ KIỆN"} islink={true} href={"eventdetail"} />
-                        ) : (
-                            <BgBlueButton content={"ĐĂNG KÝ SỰ KIỆN"} islink={false} />
-                        )
-                    }
+                <div className="pb-4 w-11/12 lg:w-4/12 drop-shadow-lg max-w-xl">
+                    <BgBlueButton content={"ĐĂNG KÝ SỰ KIỆN"} onClick={handleNavigate}/>
                 </div>
-            
             </div>
     )
 }
