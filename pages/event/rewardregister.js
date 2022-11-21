74ff8825e08a7be72958434cef270cad218b9691
Jin Eu <a11718trannhuy@gmail.com>
2022-11-21 16:09:14 +0700

    reward register add reward

--- pages/event/rewardregister.js
+++ pages/event/rewardregister.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback } from "react";
 import { useRouter } from "next/router"
 
-import AuthInput from "public/shared/AuthInput";
 import BgBlueButton from "public/shared/BgBlueButton";
-import ImageCustom from "public/shared/ImageCustom";
-import Line from "public/shared/Line";
+import Reward from "components/RewardRegister/Reward";
 import SingleColorButton from "public/shared/SingleColorButton";
 
 
@@ -14,62 +12,28 @@ function EventRewardRegister () {
     // router
     const router = useRouter()
 
+    // state
     const [giftCount, setGiftCount] = useState(1)
-    const [giftName, setGiftName] = useState("")
-    const [imgList, setImgList] = useState([])
-    const [file, setFile] = useState()
+    const [giftName, setGiftName] = useState({})
+    const [rewardList, setRewardList] = useState([])
 
-    const contentCSS = {
-        background: "-webkit-linear-gradient(45deg, #003B93, #00F0FF)",
-        WebkitBackgroundClip: "text",
-        WebkitTextFillColor: "transparent",
-    }
-    // icon plus/minus
-    const icon = {
-        width:"32px",
-        height:"32px",
-        margin:"0 12px",
-    }
-
-    const iconStyle = {
-        color:"white",
-        fontSize:"24px"
-    }
-
-    const buttonColor = {
-        background:"#40BEE5"
-    }
-
-    useEffect(() =>
+    const onChangeHandlerRewardName = (e) =>
     {
-        setImgList(prev => 
+        setGiftName(prev => ({...prev,[e.target.name]:e.target.value}))
+    }
+
+    const hanldeAddReward = () =>
+    {
+        setRewardList(prev =>
             {
-                let newImgList = []
-                if(file !== undefined)
-                {
-                    newImgList = [...prev, <ImageCustom image={file}/>]
-                }
-                return newImgList
-        })
-    },[file])
-
-    const handleChangeFile = (e) =>
-    {
-        const upload = e.target.files[0]
-        setFile(URL.createObjectURL(upload))
-
-        let toggle = document.getElementById("toggleIMG")
-        toggle.style.display = "flex"
-    }
-    
-    const getImage = (e) =>
-    {
-        document.getElementById("getFile").click()
+                let newList = [...prev, <Reward />]
+                return newList
+            })
     }
 
     const handleNavigate = () =>
     {
-        if(giftName !== "")
+        if(giftName !== null)
         {
             router.push("/event/eventdetail")
         }
@@ -79,59 +43,30 @@ function EventRewardRegister () {
             <div className="flex flex-col items-center justify-between w-screen h-screen py-2">
                 <div className="flex flex-col items-center justify-center w-full">
                     <h1 className="uppercase text-xl py-2 mb-2 font-bold text-[#004599]">thông tin giải thưởng</h1>
-                    <div className="flex w-[90%] lg:w-4/12 max-w-xl">
-                        <div className="flex flex-col justify-evenly mr-4">
-                            <i className="fas fa-angle-up cursor-pointer"></i>
-                            <i className="fas fa-angle-down cursor-pointer"></i>
-                        </div>
-                        <div className="w-full">
-                            <AuthInput content={"Tên giải thưởng"} type={"text"} onChange={(e) => setGiftName(e.target.value)}/>
-                        </div>
-                    </div>
-
-                    <div className="flex justify-evenly items-center w-full lg:w-4/12 mt-1 mb-4 lg:justify-center">
-                        <label className="font-bold" style={contentCSS}>Số lượng</label>
-                        <div className="flex justify-between items-center text-lg font-bold" style={contentCSS}>
-                            <i className="fas fa-minus px-2 flex justify-center items-center cursor-pointer" style={icon} onClick={() => setGiftCount(Math.max(giftCount - 1, 1))}></i>
-                            <label>{giftCount}</label>
-                            <i className="fas fa-plus px-2 flex justify-center items-center cursor-pointer" style={icon} onClick={() => setGiftCount(giftCount + 1)}></i>
-                        </div>
-                    </div>
-
-                    <div className="w-[90%] lg:w-4/12 max-w-xl flex mb-2">
-                        <Line content={"Hình ảnh giải thưởng"}/>
-                    </div>
-
-                    <div className="flex w-[90%] lg:w-4/12 max-w-xl mb-2 overflow-x-auto overflow-y-hidden ">
-                        <div className="w-full mb-2 hidden" id="toggleIMG">
-                            {
-                                imgList.map((item,index) =>
-                                {
-                                    return (
-                                        <div key={index} className="mx-2 ">
-                                            {item}
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    </div>
-
-                    <div className="w-[90%] lg:w-4/12 max-w-xl">
-                        <button className="flex justify-evenly items-center w-full h-[35px] rounded-[5px]" style={buttonColor} onClick={getImage}>
-                            <div className="font-[900] text-[24px] text-white">Thêm hình ảnh</div>
-                            <i className="fas fa-image" style={iconStyle}></i>
-                        </button>
-                        <input type={"file"} id={"getFile"} onChange={handleChangeFile} style={{display:"none"}}/>
-                    </div>
-
-                    <div className="w-[90%] lg:w-4/12 max-w-xl flex mb-2">
-                        <Line />
+                    <div className="overflow-y-auto overflow-x-hidden w-full max-h-[400px]">
+                        <Reward
+                            name={"field1"}
+                            valueRewardName={giftName.field1}
+                            onChangeRewardName={onChangeHandlerRewardName}
+                            valueGiftCount={giftCount}
+                            onChangeGiftCountMinus={(e) => setGiftCount(Math.max(giftCount - 1, 1))}
+                            onChangeGiftCountPlus={(e) => setGiftCount(giftCount + 1)}
+                        />
+                        <Reward
+                            name={"field2"}
+                            valueRewardName={giftName.field2}
+                            onChangeRewardName={onChangeHandlerRewardName}
+                            valueGiftCount={giftCount}
+                            onChangeGiftCountMinus={(e) => setGiftCount(Math.max(giftCount - 1, 1))}
+                            onChangeGiftCountPlus={(e) => setGiftCount(giftCount + 1)}
+                        />
+                        
+                        
                     </div>
 
                     <div className="flex justify-center items-center mb-2 mt-1 w-[90%] lg:w-4/12 max-w-xl cursor-pointer drop-shadow-lg">
-                        <SingleColorButton content={"Thêm giải thưởng"} colorHex={"#40BEE5"} />
-                    </div>
+                        <SingleColorButton content={"Thêm giải thưởng"} colorHex={"#40BEE5"} onClick={hanldeAddReward}/>
+                    </div> 
 
                 </div>
 
--- public/shared/AuthInput.js
+++ public/shared/AuthInput.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
-export default function AuthInput({ content, type, onChange, value, min }) {
+export default function AuthInput({ content, type, onChange, value, min, name }) {
   const contentCSS = {
     background: "-webkit-linear-gradient(45deg, #003B93, #00F0FF)",
     WebkitBackgroundClip: "text",
@@ -11,6 +11,7 @@ export default function AuthInput({ content, type, onChange, value, min }) {
     <div className="bg-gradient-to-r from-[#003B93] to-[#00F0FF] p-[2px] rounded-[10px] w-full h-[60px] py-[2px] my-4 outline-none relative">
       <div className="h-full">
         <input
+          name={name}
           type={type}
           className="h-full w-full rounded-lg text-lg px-4 outline-none border-none"
           onChange={onChange}
