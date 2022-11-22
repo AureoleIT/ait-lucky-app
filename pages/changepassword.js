590df8e4cd45e7f9fd239454b7ed2bcb18015296
Vũ Phạm Ngọc Tân <19520930@gm.uit.edu.vn>
2022-11-22 11:17:00 +0700

    complete FE changePw, forgotPw, get Db user

---
+++ pages/changepassword.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import Header from "public/shared/Header";
+import AuthInput from "public/shared/AuthInput";
+import BgBlueButton from "public/shared/BgBlueButton";
+import { AuthContext } from "src/context/AuthContext";
+import {
+    getDatabase,
+    ref,
+    set,
+    child,
+    get,
+    orderByKey,
+    query,
+    orderByChild,
+} from "firebase/database";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+export default function ChangePassword() {
+    const [oldPass, setOld] = useState("");
+    const [newPass, setNew] = useState("");
+    const [repeatPass, setRepeat] = useState("");
+
+    const db = getDatabase();
+    const auth = getAuth();
+
+    const handleSaveInfo = (event) => {
+        event.preventDefault();
+
+        const dbRef = ref(db);
+
+        console.log(dbRef);
+    }
+
+
+    return (
+        <section className="h-screen overflow-y-hidden">
+            <Header />
+            <div className="relative h-full ">
+                <div
+                    className="flex xl:justify-center lg:justify-center justify-center items-center h-full"
+                >
+                    <div className="absolute top-10 flex flex-col w-full max-w-md w-3/4 md:mb-0">
+                        <div className="flex flex-col justify-center items-center">
+                            <p className="text-lg mb-0 font-bold text-[#004599] mt-2 ">ĐỔI MẬT KHẨU</p>
+                        </div>
+
+                        <div className="">
+                            <AuthInput content={"Mật khẩu cũ"} type={"password"}
+                                onChange={(e) => setOld(e.target.value)} />
+                            <AuthInput content={"Mật khẩu mới"} type={"password"}
+                                onChange={(e) => setNew(e.target.value)} />
+                            <AuthInput content={"Nhập lại mật khẩu"} type={"password"}
+                                onChange={(e) => setRepeat(e.target.value)} />
+                            <BgBlueButton content={"LƯU"} islink={true} href={"/setting"} onClick={handleSaveInfo}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    );
+}
--- pages/setting.js
+++ pages/setting.js
@@ -1,15 +1,47 @@
-
-import React, { useState } from "react";
+import Auth from "layouts/Auth.js";
+import React, { useState,  useEffect } from "react";
 import Header from "public/shared/Header";
 import AuthInput from "public/shared/AuthInput";
 import BgBlueButton from "public/shared/BgBlueButton";
-import BgWhiteButton from "public/shared/BgWhiteButton";
+import Link from "next/link";
+import { AuthContext } from "src/context/AuthContext";
+import {
+    getDatabase,
+    ref,
+    set,
+    child,
+    get,
+    orderByKey,
+    query,
+    orderByChild,
+} from "firebase/database";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-export default function EventResult() {
+export default function Setting() {
+    const db = getDatabase();
+    const auth = getAuth();
+
     const gradientText = {
         background: "-webkit-linear-gradient(45deg, #003B93, #00F0FF)",
         "-webkit-background-clip": "text",
         "-webkit-text-fill-color": "transparent",
+    }
+
+    const [username, setUsername] = useState("vutan");
+    const [email, setEmail] = useState("vutan@gmail.com")
+
+    const handleSaveInfo = (event) => {
+        event.preventDefault();
+
+        const dbRef = ref(db);
+
+        get(child(dbRef, "users/")).then((snapshot) => {
+            const record = snapshot.val() ?? [];
+            const values = Object.values(record);
+            
+            console.log(values);
+            });
+
     }
 
     return (
@@ -31,20 +63,31 @@ export default function EventResult() {
                         </div>
 
                         <div className="">
-                            <AuthInput content={"Tên đăng nhập"} />
-                            <AuthInput content={"Email"} />
-                            <BgBlueButton content={"LƯU"} islink={true} href={"/"} />
+                            <AuthInput
+                                content={"Tên đăng nhập"}
+                                type={"text"}
+                                onChange={(e) => setUsername(e.target.value)}
+                                value={username} />
+                            <AuthInput
+                                content={"Email"}
+                                type={"email"}
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email} />
+                            <BgBlueButton content={"LƯU"} islink={true} href={"/"} onClick={handleSaveInfo} />
                         </div>
 
                         <div className="absolute bottom-20 w-full max-w-md w-3/4  text-center lg:text-left ">
                             <div className="w-full h-[50px] rounded-[50px] bg-gradient-to-r from-[#003B93] to-[#00F0FF] p-[2px]">
-                                <button className="w-full h-full rounded-[48px] bg-white flex items-center justify-center gap-[10px]" >
-                                    <p className="font-[900] text-[22px] text-white" style={gradientText}>ĐỔI MẬT KHẨU</p>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="lightblue" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                    </svg>
+                                <Link href={"/changepassword"}>
 
-                                </button>
+                                    <button className="w-full h-full rounded-[48px] bg-white flex items-center justify-center gap-[10px]">
+                                        <p className="font-[900] text-[22px] text-white" style={gradientText}>ĐỔI MẬT KHẨU</p>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="lightblue" className="w-6 h-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                        </svg>
+
+                                    </button>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -54,3 +97,5 @@ export default function EventResult() {
 
     );
 }
+
+Setting.layout = Auth;
