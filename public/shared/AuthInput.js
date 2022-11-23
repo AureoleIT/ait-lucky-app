6f6491bac7e6c6013624a2c4b11fe319f63f2a11
vietnh <vieta3pro380@gmail.com>
2022-11-23 08:54:05 +0700

    no message

--- public/shared/AuthInput.js
+++ public/shared/AuthInput.js
@@ -1,23 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function AuthInput({ content, type, onChange, value, min, name }) {
+export default function AuthInput({ content, type, onChange, value }) {
   const contentCSS = {
     background: "-webkit-linear-gradient(45deg, #003B93, #00F0FF)",
-    WebkitBackgroundClip: "text",
-    WebkitTextFillColor: "transparent",
+    "-webkit-background-clip": "text",
+    "-webkit-text-fill-color": "transparent",
   };
 
   return (
     <div className="bg-gradient-to-r from-[#003B93] to-[#00F0FF] p-[2px] rounded-[10px] w-full h-[60px] py-[2px] my-4 outline-none relative">
       <div className="h-full">
         <input
-          name={name}
           type={type}
           className="h-full w-full rounded-lg text-lg px-4 outline-none border-none"
           onChange={onChange}
           value={value}
           required
-          min={min}
         />
         <div className="bg-white absolute w-full top-0">
           <label
