11f492d3274452739ab96c0d1de6da0b94c2d50e
Nguyen Anh Tuan <77793919+ng-anhhtuann@users.noreply.github.com>
2022-11-24 16:00:50 +0700

    Bug fix in webkit-pack in popup and buttons

--- public/shared/PopUp.js
+++ public/shared/PopUp.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { TEXT } from "public/util/colors";
 import ClosePopUp from "./ClosePopUp";
-export default function PopUp({text, icon, close}) {
+export default function PopUp({text, icon, close, isWarning}) {
+  const bg = isWarning ? "border-red-600" : "border-green-600"; 
   return (
     <div
-      className="h-1/4 w-3/4 max-w-md bg-white border-2 p-5 pb-16 border-slate-500 
-           shadow-lg shadow-slate-500 rounded-3xl flex flex-col 
-           align-middle justify-between"
+      className={`h-1/4 w-3/4 max-w-md bg-white border-4 p-5 pb-16 ${bg} 
+      shadow-xl shadow-slate-500 rounded-3xl flex flex-col align-middle justify-between`}
     >
       <ClosePopUp closeAction={close}/>
       <span
