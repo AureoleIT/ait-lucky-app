7f650bb99662f958726afa5850fdf2bbb9067d2c
thuyntc <camthuy0212@gmail.com>
2022-11-25 16:38:10 +0700

    get event data from firebase

--- pages/admin/event-list.js
+++ pages/admin/event-list.js
@@ -1,140 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "public/shared/Header";
 import AuthInput from "public/shared/AuthInput";
 import EventButton from "public/shared/button/EventButton";
 import { LEFT_COLOR, RIGHT_COLOR } from "public/util/colors";
-
-// DB test
-const event = [
-  {
-    id: "EV20221011",
-    title: "tiệc cuối năm",
-    user_joined: 10,
-    status: 1,
-  },
-  {
-    id: "EV20221012",
-    title: "tiệc năm mới",
-    user_joined: 20,
-    status: 2,
-  },
-  {
-    id: "EV20221013",
-    title: "tiệc thành lập công ty trách nhiệm hữu hạn",
-    user_joined: 30,
-    status: 3,
-  },
-  {
-    id: "EV20221014",
-    title: "cuối năm",
-    user_joined: 40,
-    status: 4,
-  },
-  {
-    id: "EV20221011",
-    title: "tiệc cuối năm",
-    user_joined: 10,
-    status: 1,
-  },
-  {
-    id: "EV20221012",
-    title: "tiệc năm mới",
-    user_joined: 20,
-    status: 2,
-  },
-  {
-    id: "EV20221013",
-    title: "tiệc thành lập công ty trách nhiệm hữu hạn",
-    user_joined: 30,
-    status: 3,
-  },
-  {
-    id: "EV20221014",
-    title: "cuối năm",
-    user_joined: 40,
-    status: 4,
-  },
-  {
-    id: "EV20221011",
-    title: "tiệc cuối năm",
-    user_joined: 10,
-    status: 1,
-  },
-  {
-    id: "EV20221012",
-    title: "tiệc năm mới",
-    user_joined: 20,
-    status: 2,
-  },
-  {
-    id: "EV20221013",
-    title: "tiệc thành lập công ty trách nhiệm hữu hạn",
-    user_joined: 30,
-    status: 3,
-  },
-  {
-    id: "EV20221014",
-    title: "cuối năm",
-    user_joined: 40,
-    status: 4,
-  },
-  {
-    id: "EV20221011",
-    title: "tiệc cuối năm",
-    user_joined: 10,
-    status: 1,
-  },
-  {
-    id: "EV20221012",
-    title: "tiệc năm mới",
-    user_joined: 20,
-    status: 2,
-  },
-  {
-    id: "EV20221013",
-    title: "tiệc thành lập công ty trách nhiệm hữu hạn",
-    user_joined: 30,
-    status: 3,
-  },
-  {
-    id: "EV20221014",
-    title: "cuối năm",
-    user_joined: 40,
-    status: 4,
-  },
-  {
-    id: "EV20221011",
-    title: "tiệc cuối năm",
-    user_joined: 10,
-    status: 1,
-  },
-  {
-    id: "EV20221012",
-    title: "tiệc năm mới",
-    user_joined: 20,
-    status: 2,
-  },
-  {
-    id: "EV20221013",
-    title: "tiệc thành lập công ty trách nhiệm hữu hạn",
-    user_joined: 30,
-    status: 3,
-  },
-  {
-    id: "EV20221014",
-    title: "cuối năm",
-    user_joined: 40,
-    status: 4,
-  },
-];
+import { db } from "src/firebase";
+import { ref, onValue, 
+  // child, get 
+} from "firebase/database";
 
 export default function EventList() {
   const [searchContent, setSearchContent] = useState("");
-      
+  const [events, setEvents] = useState([]);
+
+  // useEffect(() => {
+  //   get(child(ref(db), "event/")).then((snapshot) => {
+  //     setEvents([]);
+  //     const data = snapshot.val();
+  //     if (data !== null) {
+  //       setEvents(Object.values(data));
+  //     }
+  //   });
+  // }, []);
+
+  useEffect(() => {
+    onValue(ref(db, "event/"), (snapshot) => {
+      setEvents([]);
+      const data = snapshot.val();
+      if (data !== null) {
+        setEvents(Object.values(data));
+      }
+    });
+  }, []);
+
   return (
     <>
-    
       <Header />
       <section className="h-screen max-w-md mx-auto flex flex-col justify-center items-center">
         <div className="flex flex-col px-3 py-5 w-full h-full items-center">
@@ -152,12 +52,12 @@ export default function EventList() {
               />
             </div>
             <div className="flex flex-col gap-y-[7px] font-[Nunito Sans] font-bold overflow-auto max-h-[700px] mb-3">
-              {event.filter((item) => {
+              {events.filter((item) => {
                 return searchContent.toLowerCase() === ""
                   ? item
                   : item.title.toLowerCase().includes(searchContent);
               }).length !== 0 ? (
-                event
+                events
                   .filter((item) => {
                     return searchContent.toLowerCase() === ""
                       ? item
