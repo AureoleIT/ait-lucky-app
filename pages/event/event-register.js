66eef377a8f5d36fb8ddba5583386785caaffa6b
Nguyen Anh Tuan <77793919+ng-anhhtuann@users.noreply.github.com>
2022-11-28 15:01:10 +0700

    Re-format inline code

--- pages/event/event-register.js
+++ pages/event/event-register.js
@@ -1,5 +1,5 @@
-import React, {useState} from "react";
-import { useRouter } from "next/router"
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 
 import TextArea from "public/shared/TextArea";
 import AuthInput from "public/shared/AuthInput";
@@ -7,75 +7,90 @@ import BgBlueButton from "public/shared/BgBlueButton";
 import Header from "public/shared/Header";
 
 export default function EventRegister() {
+  // router
+  const router = useRouter();
 
-    // router
-    const router = useRouter()
+  const [nameEvent, setNameEvent] = useState("");
+  const [eventDetail, setEventDetail] = useState("");
+  const [limitUser, setLimitUser] = useState("");
 
-    const [nameEvent, setNameEvent] = useState("")
-    const [eventDetail, setEventDetail] = useState("")
-    const [limitUser, setLimitUser] = useState("")
+  const contentCSS = {
+    background: "-webkit-linear-gradient(45deg, #003B93, #00F0FF)",
+    WebkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+  };
 
-    const contentCSS = {
-        background: "-webkit-linear-gradient(45deg, #003B93, #00F0FF)",
-        WebkitBackgroundClip: "text",
-        WebkitTextFillColor: "transparent",
+  // handle submit button
+  const handleSubmit = () => {
+    if (nameEvent !== "" && eventDetail !== "" && limitUser !== "") {
+      alert("Thanh cong !!!");
+      router.push("/event/reward-register");
+    } else {
+      alert("Vui lòng nhập đủ thông tin !!!");
     }
+  };
 
-    // handle submit button
-    const handleSubmit = () =>
-    {
-        if(nameEvent !== "" && eventDetail !== "" && limitUser !== "")
-        {
-            alert("Thanh cong !!!")
-            router.push("/event/reward-register")
+  return (
+    <>
+      <div className="flex flex-col overflow-y-auto overflow-x-hidden items-center justify-between h-screen w-screen">
+        <div className="w-full">
+          <Header />
+        </div>
+        <div className="flex flex-col items-center justify-center w-full">
+          <h1 className="uppercase text-4xl py-3 font-bold text-[#004599]">
+            đăng ký
+          </h1>
+          <h1 className="uppercase text-xl py-3 font-bold text-[#004599] mb-4">
+            thông tin sự kiện
+          </h1>
 
-        }
-        else 
-        {
-            alert("Vui lòng nhập đủ thông tin !!!")
-        }
-    }
+          <div className="w-3/4 lg:w-4/12">
+            <TextArea
+              content={"Tên sự kiện"}
+              value={nameEvent}
+              onChange={(e) => setNameEvent(e.target.value)}
+            />
+          </div>
 
-    return (
-        <>
-            <div className="flex flex-col overflow-y-auto overflow-x-hidden items-center justify-between h-screen w-screen">
-                <div className="w-full">
-                    <Header />
-                </div>
-                <div className="flex flex-col items-center justify-center w-full">
-                    <h1 className="uppercase text-4xl py-3 font-bold text-[#004599]">đăng ký</h1>
-                    <h1 className="uppercase text-xl py-3 font-bold text-[#004599] mb-4">thông tin sự kiện</h1>
-                    
-                    <div className="w-3/4 lg:w-4/12">
-                        <TextArea content={"Tên sự kiện"} value={nameEvent} onChange={(e) => setNameEvent(e.target.value)}/>
-                    </div>
+          <div className="pb-[1rem]  pt-[2rem] w-3/4 lg:w-4/12">
+            <TextArea
+              content={"Mô tả sự kiện"}
+              row={5}
+              value={eventDetail}
+              onChange={(e) => setEventDetail(e.target.value)}
+            />
+          </div>
 
-                    <div className="pb-[1rem]  pt-[2rem] w-3/4 lg:w-4/12">
-                        <TextArea content={"Mô tả sự kiện"} row={5} value={eventDetail} onChange={(e) => setEventDetail(e.target.value)} />
-                    </div>
+          <div className="w-3/4 lg:w-4/12">
+            <AuthInput
+              content={"Giới hạn người tham gia"}
+              type={"number"}
+              min={"1"}
+              value={limitUser}
+              onChange={(e) => setLimitUser(e.target.value)}
+            />
+          </div>
 
-                    <div className="w-3/4 lg:w-4/12">
-                        <AuthInput content={"Giới hạn người tham gia"} type={"number"} min={"1"} value={limitUser} onChange={(e) => setLimitUser(e.target.value)}/>
-                    </div>
-
-                    <div className="w-3/4 lg:w-4/12 flex ">
-                        <div className="w-[70%]">
-                            <p style={contentCSS} className="font-bold">Cho phép người tham gia không cần đăng nhập</p>
-                        </div>
-                        <div className="w-[30%] flex justify-end items-center">
-                            <input
-                                type="checkbox"
-                                id="accept"
-                                className="appearance-none w-9 focus:outline-none checked:bg-blue-300 h-5 bg-[#ccc] rounded-full before:inline-block before:rounded-full before:bg-blue-500 before:h-4 before:w-4 checked:before:translate-x-full shadow-inner transition-all duration-300 before:ml-0.5"
-                            />
-                        </div>
-                    </div>
-                </div>
-
-                <div className="py-3 w-3/4 lg:w-4/12" onClick={handleSubmit}>
-                    <BgBlueButton content={"TIẾP TỤC"} />
-                </div>
+          <div className="w-3/4 lg:w-4/12 flex ">
+            <div className="w-[70%]">
+              <p style={contentCSS} className="font-bold">
+                Cho phép người tham gia không cần đăng nhập
+              </p>
             </div>
-        </>
+            <div className="w-[30%] flex justify-end items-center">
+              <input
+                type="checkbox"
+                id="accept"
+                className="appearance-none w-9 focus:outline-none checked:bg-blue-300 h-5 bg-[#ccc] rounded-full before:inline-block before:rounded-full before:bg-blue-500 before:h-4 before:w-4 checked:before:translate-x-full shadow-inner transition-all duration-300 before:ml-0.5"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="py-3 w-3/4 lg:w-4/12" onClick={handleSubmit}>
+          <BgBlueButton content={"TIẾP TỤC"} />
+        </div>
+      </div>
+    </>
   );
 }
--- pages/event/lucky_spin.js
+++ pages/event/lucky_spin.js
@@ -20,128 +20,183 @@ import CurrentEventDetail from "public/shared/CurrentEventDetail";
 
 // DB test
 const listPlayer = [
-    {
-        playerName: "Nguyễn Văn A",
-        playerAvt: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-    }, {
-        playerName: "Nguyễn Văn B",
-        playerAvt: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-    }, {
-        playerName: "Nguyễn Văn C",
-        playerAvt: "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-    }, {
-        playerName: "Nguyễn Văn D",
-        playerAvt: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-    }, {
-        playerName: "Nguyễn Văn E",
-        playerAvt: "https://images.unsplash.com/photo-1628890920690-9e29d0019b9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-    }, {
-        playerName: "Nguyễn Văn F",
-        playerAvt: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-    }, {
-        playerName: "Nguyễn Văn G",
-        playerAvt: "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-    }, {
-        playerName: "Nguyễn Văn H",
-        playerAvt: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-    }, {
-        playerName: "Nguyễn Văn I",
-        playerAvt: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-    }
+  {
+    playerName: "Nguyễn Văn A",
+    playerAvt:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    playerName: "Nguyễn Văn B",
+    playerAvt:
+      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    playerName: "Nguyễn Văn C",
+    playerAvt:
+      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    playerName: "Nguyễn Văn D",
+    playerAvt:
+      "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    playerName: "Nguyễn Văn E",
+    playerAvt:
+      "https://images.unsplash.com/photo-1628890920690-9e29d0019b9b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    playerName: "Nguyễn Văn F",
+    playerAvt:
+      "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGF2YXRhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    playerName: "Nguyễn Văn G",
+    playerAvt:
+      "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    playerName: "Nguyễn Văn H",
+    playerAvt:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    playerName: "Nguyễn Văn I",
+    playerAvt:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  },
 ];
 const listReward = [
-    {
-        id: "",
-        event_id: "",
-        description: "Giải Đặc Biệt",
-        quantity: 1,
-        quantity_remain: 1,
-        sort_no: 0,
-        img_url: [
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-        ]
-    }, {
-        id: "",
-        event_id: "",
-        description: "Giải Nhất",
-        quantity: 1,
-        quantity_remain: 1,
-        sort_no: 0,
-        img_url: [
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI="
-        ]
-    }, {
-        id: "",
-        event_id: "",
-        description: "Giải Nhì",
-        quantity: 1,
-        quantity_remain: 1,
-        sort_no: 0,
-        img_url: [
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-        ]
-    }, {
-        id: "",
-        event_id: "",
-        description: "Giải Ba",
-        quantity: 1,
-        quantity_remain: 1,
-        sort_no: 0,
-        img_url: [
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-            "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
-        ]
-    }
+  {
+    id: "",
+    event_id: "",
+    description: "Giải Đặc Biệt",
+    quantity: 1,
+    quantity_remain: 1,
+    sort_no: 0,
+    img_url: [
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+    ],
+  },
+  {
+    id: "",
+    event_id: "",
+    description: "Giải Nhất",
+    quantity: 1,
+    quantity_remain: 1,
+    sort_no: 0,
+    img_url: [
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+    ],
+  },
+  {
+    id: "",
+    event_id: "",
+    description: "Giải Nhì",
+    quantity: 1,
+    quantity_remain: 1,
+    sort_no: 0,
+    img_url: [
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+    ],
+  },
+  {
+    id: "",
+    event_id: "",
+    description: "Giải Ba",
+    quantity: 1,
+    quantity_remain: 1,
+    sort_no: 0,
+    img_url: [
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+      "https://media.istockphoto.com/id/1307086567/photo/generic-modern-suv-car-in-concrete-garage.jpg?b=1&s=170667a&w=0&k=20&c=m2g-wU5m2tbqC7C_nWAgu7txHzeEnXKSFuby01V4dtI=",
+    ],
+  },
 ];
 
 export default function LuckySpin() {
-    const [playerAmount, setPlayerAmount] = useState(listPlayer.length);
-    const [rewardChosing, setRewardChosing] = useState(0);
+  const [playerAmount, setPlayerAmount] = useState(listPlayer.length);
+  const [rewardChosing, setRewardChosing] = useState(0);
 
-    return (
-        <>
-            <section className="relative h-screen px-5 py-5 mx-auto flex justify-center items-center w-3/4 max-w-md max-sm:w-full">
-                <div className="flex flex-col justify-start items-center w-full h-full">
-                    <div className="flex flex-col w-full pt-5">
-                        <Title title="QUAY THƯỞNG MAY MẮN" fontSize="24" fontWeight="semibold"/>
-                        <Title title="TIỆC CUỐI NĂM" fontSize="32" />
-                        <div className="flex w-full justify-between -mt-3 mb-1">
-                            <p className="font-[900] text-[#004599] text-[16px] text-left items-center h-6">Số người trực tuyến</p>
-                            <span className="flex gap-1">
-                                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">{Math.floor(playerAmount/100)}</p>
-                                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">{Math.floor((playerAmount%100)/10)}</p>
-                                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">{Math.floor((playerAmount%100)%10)}</p>
-                            </span>
-                        </div>
-                        <div className="flex w-full justify-between">
-                            <p className="font-[900] text-[#004599] text-[16px] text-left items-center h-6">Số người quay thưởng</p>
-                            <span className="flex gap-1">
-                                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">{Math.floor(playerAmount/100)}</p>
-                                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">{Math.floor((playerAmount%100)/10)}</p>
-                                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">{Math.floor((playerAmount%100)%10)}</p>
-                            </span>
-                        </div>
-                    </div>
-                    <Spin listPlayer={listPlayer} />
-                    <div className="w-full mb-12">
-                        <p className="font-[900] text-[#004599] uppercase text-[16px] text-center items-center">giải thưởng hiện tại</p>
-                        <div className="h-44 px-4 py-2 relative">
-                            <RewardList listReward={listReward[0]} showRemain={true} showAwardedPaticipant={true} />
-                        </div>
-                    </div>
-                    <div className="absolute right-2 top-2 rounded-full h-10 w-10 bg-gradient-to-r from-[#003B93] to-[#00F0FF] p-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 fill-white">
-                            <path fillRule="evenodd" d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z" clipRule="evenodd" />
-                        </svg>
-                    </div>
-                    <CurrentEventDetail listPlayer={listPlayer} listReward={listReward} />
-                </div>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className="relative h-screen px-5 py-5 mx-auto flex justify-center items-center w-3/4 max-w-md max-sm:w-full">
+        <div className="flex flex-col justify-start items-center w-full h-full">
+          <div className="flex flex-col w-full pt-5">
+            <Title
+              title="QUAY THƯỞNG MAY MẮN"
+              fontSize="24"
+              fontWeight="semibold"
+            />
+            <Title title="TIỆC CUỐI NĂM" fontSize="32" />
+            <div className="flex w-full justify-between -mt-3 mb-1">
+              <p className="font-[900] text-[#004599] text-[16px] text-left items-center h-6">
+                Số người trực tuyến
+              </p>
+              <span className="flex gap-1">
+                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">
+                  {Math.floor(playerAmount / 100)}
+                </p>
+                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">
+                  {Math.floor((playerAmount % 100) / 10)}
+                </p>
+                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">
+                  {Math.floor((playerAmount % 100) % 10)}
+                </p>
+              </span>
+            </div>
+            <div className="flex w-full justify-between">
+              <p className="font-[900] text-[#004599] text-[16px] text-left items-center h-6">
+                Số người quay thưởng
+              </p>
+              <span className="flex gap-1">
+                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">
+                  {Math.floor(playerAmount / 100)}
+                </p>
+                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">
+                  {Math.floor((playerAmount % 100) / 10)}
+                </p>
+                <p className="items-center text-center bg-[#3B88C3] text-white font-[900] rounded-md w-6 h-6">
+                  {Math.floor((playerAmount % 100) % 10)}
+                </p>
+              </span>
+            </div>
+          </div>
+          <Spin listPlayer={listPlayer} />
+          <div className="w-full mb-12">
+            <p className="font-[900] text-[#004599] uppercase text-[16px] text-center items-center">
+              giải thưởng hiện tại
+            </p>
+            <div className="h-44 px-4 py-2 relative">
+              <RewardList
+                listReward={listReward[0]}
+                showRemain={true}
+                showAwardedPaticipant={true}
+              />
+            </div>
+          </div>
+          <div className="absolute right-2 top-2 rounded-full h-10 w-10 bg-gradient-to-r from-[#003B93] to-[#00F0FF] p-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-8 h-8 fill-white"
+            >
+              <path
+                fillRule="evenodd"
+                d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 00-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 00-2.282.819l-.922 1.597a1.875 1.875 0 00.432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 000 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 00-.432 2.385l.922 1.597a1.875 1.875 0 002.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 002.28-.819l.923-1.597a1.875 1.875 0 00-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 000-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 00-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 00-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 00-1.85-1.567h-1.843zM12 15.75a3.75 3.75 0 100-7.5 3.75 3.75 0 000 7.5z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </div>
+          <CurrentEventDetail listPlayer={listPlayer} listReward={listReward} />
+        </div>
+      </section>
+    </>
+  );
 }
