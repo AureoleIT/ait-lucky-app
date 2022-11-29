73d708131afc267d2b540d20774fb88d8ace40bc
phongptt <pttp1512@gmail.com>
2022-11-29 11:07:35 +0700

    Use next/link instead of normal img tag

--- public/shared/UserAvatar.js
+++ public/shared/UserAvatar.js
@@ -1,14 +1,22 @@
 import Link from "next/link";
+import Image from "next/image"
 import React from "react";
 
 export default function UserAvatar({ avatar }) {
+  const myLoader = ({src, width, quality}) => {
+    return avatar
+  }
+
   return (
     <Link href="/auth/setting">
       <div className="w-[40px] h-[40px]">
-        <img
-          src={avatar}
-          alt=""
-          className="rounded-full w-[40px] h-[40px] object-cover"
+        <Image
+          loader={myLoader}
+          src={"avatar.png"}
+          alt="User's avatar"
+          width={40}
+          height={40}
+          className="rounded-full object-cover"
         />
       </div>
     </Link>
