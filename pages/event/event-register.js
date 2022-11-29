0d0811a64c2ff0f1fad8654a777a3dece010a1d1
Jin Eu <a11718trannhuy@gmail.com>
2022-11-29 21:19:08 +0700

    optimize and and split to component

--- pages/event/event-register.js
+++ pages/event/event-register.js
@@ -6,6 +6,7 @@ import BgBlueButton from "public/shared/BgBlueButton";
 import Header from "public/shared/Header";
 import { failIcon, hidden, show, successIcon } from "public/util/popup";
 import PopUp from "public/shared/PopUp";
+import CheckBox from "public/shared/CheckBox";
 
 export default function EventRegister() {
     // router
@@ -57,52 +58,48 @@ export default function EventRegister() {
                 <div className="w-full">
                     <Header />
                 </div>
-                <div className="flex flex-col items-center justify-center w-full">
-                    <h1 className="uppercase text-4xl py-3 font-bold text-[#004599]">
-                        đăng ký
-                    </h1>
-                    <h1 className="uppercase text-xl py-3 font-bold text-[#004599] mb-4">
-                        thông tin sự kiện
-                    </h1>
-
-                    <div className="w-3/4 lg:w-4/12">
-                        <TextArea
-                            content={"Tên sự kiện"}
-                            ref={nameEventRef}
-                        />
-                    </div>
-                    <div className="pb-[1rem]  pt-[2rem] w-3/4 lg:w-4/12">
-                        <TextArea
-                            content={"Mô tả sự kiện"}
-                            row={5}
-                            ref={eventDetailRef}
-                        />
-                    </div>
-
-                    <div className="w-3/4 lg:w-4/12">
-                        <AuthInput
-                            leftColor={"#003B93"}
-                            rightColor={"#00F0FF"}
-                            content={"Giới hạn người tham gia"}
-                            type={"number"}
-                            min={"1"}
-                            ref={limitUserRef}
-                        />
-                    </div>
-
-                    <div className="w-3/4 lg:w-4/12 flex ">
-                        <div className="w-[70%]">
-                            <p style={contentCSS} className="font-bold">
-                                Cho phép người tham gia không cần đăng nhập
-                            </p>
-                        </div>
-                        <div className="w-[30%] flex justify-end items-center">
-                            <input
-                                ref={checkBoxRef}
-                                type="checkbox"
-                                id="accept"
-                                className="appearance-none w-9 focus:outline-none checked:bg-blue-300 h-5 bg-[#ccc] rounded-full before:inline-block before:rounded-full before:bg-blue-500 before:h-4 before:w-4 checked:before:translate-x-full shadow-inner transition-all duration-300 before:ml-0.5"
+                <div className="w-full flex items-center justify-center">
+                    <div className="flex flex-col items-center justify-center w-3/4 lg:w-4/12">
+                        <h1 className="uppercase text-4xl py-3 font-bold text-[#004599]">
+                            đăng ký
+                        </h1>
+                        <h1 className="uppercase text-xl py-3 font-bold text-[#004599] mb-4">
+                            thông tin sự kiện
+                        </h1>
+                        <div className="w-full h-[70px]">
+                            <TextArea
+                                content={"Tên sự kiện"}
+                                maxLength={"100"}
+                                ref={nameEventRef}
                             />
+                        </div>
+                        <div className="pb-[1rem] pt-[2rem] w-full h-[200px]">
+                            <TextArea
+                                content={"Mô tả sự kiện"}
+                                row={5}
+                                maxLength={"100"}
+                                ref={eventDetailRef}
+                            />
+                        </div>
+                        <div className="w-full">
+                            <AuthInput
+                                leftColor={"#003B93"}
+                                rightColor={"#00F0FF"}
+                                content={"Giới hạn người tham gia"}
+                                type={"number"}
+                                min={"1"}
+                                ref={limitUserRef}
+                            />
+                        </div>
+                        <div className="w-full flex justify-center items-center">
+                            <div className="w-[70%]">
+                                <p style={contentCSS} className="font-bold">
+                                    Cho phép người tham gia không cần đăng nhập
+                                </p>
+                            </div>
+                            <div className="w-[30%] flex items-center text-right">
+                                <CheckBox ref={checkBoxRef} />
+                            </div>
                         </div>
                     </div>
                 </div>
