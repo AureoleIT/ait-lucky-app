975ef2d8c9105a4e571dc0482458cc35fcc5580b
Vũ Phạm Ngọc Tân <19520930@gm.uit.edu.vn>
2022-11-30 09:24:34 +0700

    complete validation

--- pages/auth/change-password.js
+++ pages/auth/change-password.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "public/shared/Header";
 import AuthInput from "public/shared/AuthInput";
 import BgBlueButton from "public/shared/BgBlueButton";
-import { LEFT_COLOR, RIGHT_COLOR } from "public/util/colors";
+import { LEFT_COLOR, RIGHT_COLOR, FAIL_RIGHT_COLOR } from "public/util/colors";
 import {
   getDatabase,
   ref,
@@ -14,8 +14,11 @@ import {
   DataSnapshot
 } from "firebase/database";
 import { useAuth } from "src/context/AuthContext";
+import { successIcon, failIcon } from "public/util/popup";
 import { isEmpty, enoughNumCountPass, hasWhiteSpaceAndValidLength } from "public/util/functions";
 import { messagesError, messagesSuccess } from "public/util/messages"
+import OverlayBlock from "public/shared/OverlayBlock";
+
 
 export default function ChangePassword() {
   const db = getDatabase();
@@ -24,30 +27,45 @@ export default function ChangePassword() {
   const [newPass, setNew] = useState("");
   const [repeatPass, setRepeat] = useState("");
 
+  //validation const
+  const [textState, setTextState] = useState("");
+  const [isHidden, setIsHidden] = useState(true);
+  const [isSuccess, setIsSuccess] = useState(true);
+
 
   const changePassword = () => {
     if (isEmpty(oldPass) || isEmpty(newPass) || isEmpty(repeatPass)) {
-      console.log(messagesError.E0004);
+      setTextState(messagesError.E0004);
+      setIsSuccess(false);
+      setIsHidden(false);
       return;
     }
 
-    if (enoughNumCountPass(oldPass))
-    {
-      console.Console(messagesError.E0005("mật khẩu cũ"))
+    if (hasWhiteSpaceAndValidLength(oldPass)) {
+      setTextState(messagesError.E0005("mật khẩu cũ"));
+      setIsSuccess(false);
+      setIsHidden(false);
+      return;
     }
 
-    if (enoughNumCountPass(newPass))
-    {
-      console.Console(messagesError.E0005("mật khẩu mới"))
+    if (hasWhiteSpaceAndValidLength(newPass)) {
+      setTextState(messagesError.E0005("mật khẩu mới"));
+      setIsSuccess(false);
+      setIsHidden(false);
+      return;
     }
 
-    if (enoughNumCountPass(repeatPass))
-    {
-      console.Console(messagesError.E0005("nhập lại mật khẩu"))
+    if (hasWhiteSpaceAndValidLength(repeatPass)) {
+      setTextState(messagesError.E0005("nhập lại mật khẩu"));
+      setIsSuccess(false);
+      setIsHidden(false);
+      return;
     }
 
     if (newPass != repeatPass) {
-      console.log(messagesError.E0021("mật khẩu mới", "nhập lại mật khẩu"));
+      setTextState(messagesError.E0021("mật khẩu mới", "nhập lại mật khẩu"));
+      setIsSuccess(false);
+      setIsHidden(false);
       return;
     }
 
@@ -58,7 +76,9 @@ export default function ChangePassword() {
       const values = Object.values(record);
 
       if (values[0].password != oldPass) {
-        console.log(messagesError.E0011("Mật khẩu cũ"));
+        setTextState(messagesError.E0011("Mật khẩu cũ"));
+        setIsSuccess(false);
+        setIsHidden(false);
         return;
       }
 
@@ -66,12 +86,50 @@ export default function ChangePassword() {
         {
           password: newPass
         }).then(() => {
-          console.log(messagesSuccess.I0003);
+          setTextState(messagesSuccess.I0003);
+          setIsSuccess(true);
+          setIsHidden(false);
+          return;
         })
-        .catch((error) => console.log(messagesError.E1002));
+        .catch((error) => {
+          setTextState(error);
+          setIsSuccess(false);
+          setIsHidden(false);
+          return;
+        });
     })
   }
 
+  // show popup
+  useEffect(() => {
+    console.log(isHidden)
+    if (isHidden == false) {
+      isSuccess ? document.getElementById("imgPopup").src = successIcon : document.getElementById("imgPopup").src = failIcon;
+      document.getElementById("textState").innerHTML = textState;
+      document.getElementById("changeOverlay").classList.toggle('hidden');
+      const timer = setTimeout(() => {
+        setIsHidden(true);
+      }, 1000)
+      return () => { clearTimeout(timer) }
+    }
+  }, [isHidden])
+
+
+  const popupNoti = () => {
+    return (
+      <div className="flex flex-col items-center">
+        <div className="text-center text-[#004599]">
+          <p className="font-[900] text-lg" id="textState"></p>
+        </div>
+        <img
+          id="imgPopup"
+          alt="success"
+          src={failIcon}
+          className="self-center w-12"
+        ></img>
+      </div>
+    )
+  }
 
   return (
     <section className="h-screen overflow-y-hidden">
@@ -88,15 +146,15 @@ export default function ChangePassword() {
             <div className="">
               <AuthInput content={"Mật khẩu cũ"} type={"password"}
                 leftColor={LEFT_COLOR}
-                rightColor={RIGHT_COLOR}
+                rightColor={!hasWhiteSpaceAndValidLength(oldPass) ? RIGHT_COLOR : FAIL_RIGHT_COLOR}
                 onChange={(e) => setOld(e.target.value)} />
               <AuthInput content={"Mật khẩu mới"} type={"password"}
                 leftColor={LEFT_COLOR}
-                rightColor={RIGHT_COLOR}
+                rightColor={!hasWhiteSpaceAndValidLength(newPass) ? RIGHT_COLOR : FAIL_RIGHT_COLOR}
                 onChange={(e) => setNew(e.target.value)} />
               <AuthInput content={"Nhập lại mật khẩu"} type={"password"}
                 leftColor={LEFT_COLOR}
-                rightColor={RIGHT_COLOR}
+                rightColor={!hasWhiteSpaceAndValidLength(repeatPass) ? RIGHT_COLOR : FAIL_RIGHT_COLOR}
                 onChange={(e) => setRepeat(e.target.value)} />
               <BgBlueButton content={"LƯU"} onClick={() => changePassword()} />
             </div>
@@ -105,22 +163,8 @@ export default function ChangePassword() {
       </div>
 
       <div className="">
-        <AuthInput
-          content={"Mật khẩu cũ"}
-          type={"password"}
-          onChange={(e) => setOld(e.target.value)}
-        />
-        <AuthInput
-          content={"Mật khẩu mới"}
-          type={"password"}
-          onChange={(e) => setNew(e.target.value)}
-        />
-        <AuthInput
-          content={"Nhập lại mật khẩu"}
-          type={"password"}
-          onChange={(e) => setRepeat(e.target.value)}
-        />
         <BgBlueButton content={"LƯU"} onClick={() => changePassword()} />
+        <OverlayBlock childDiv={popupNoti()} id={"changeOverlay"} />
       </div>
     </section>
   );
