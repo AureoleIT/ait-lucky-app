591e412ab049d4a7fed0ae673346551cd6f87f5d
ytran30 <a11718trannhuy@gmail.com>
2022-12-01 14:32:13 +0700

    change path file

--- pages/admin/event/event-register.js
+++ pages/admin/event/event-register.js
@@ -83,7 +83,7 @@ export default function EventRegister() {
         });
 
       setTimeout(() => {
-        router.push("/event/reward-register");
+        router.push("/admin/event/reward-register");
       }, 3000);
     }
   };
