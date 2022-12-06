2a3769ba6d1f02584e415cbfccdf82112d07360a
Vũ Phạm Ngọc Tân <19520930@gm.uit.edu.vn>
2022-12-06 13:35:15 +0700

    import db from firebase.js, useMeno to set state validation

--- pages/auth/forgot-password.js
+++ pages/auth/forgot-password.js
@@ -1,5 +1,5 @@
 import Auth from "layouts/Auth.js";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import AuthInput from "public/shared/AuthInput";
 import BgBlueButton from "public/shared/BgBlueButton";
 import GradientLine from "public/shared/GradientLine";
@@ -8,50 +8,47 @@ import AuthFooter from "public/shared/AuthFooter";
 import Link from "next/link";
 import { useAuth } from "src/context/AuthContext";
 import {
-  getDatabase,
   ref,
   query,
   orderByChild,
   equalTo,
   update,
   onValue,
-  DataSnapshot
 } from "firebase/database";
 import { LEFT_COLOR, RIGHT_COLOR, FAIL_RIGHT_COLOR } from "public/util/colors";
 import { successIcon, failIcon } from "public/util/popup";
 import OverlayBlock from "public/shared/OverlayBlock";
 import { isEmpty, hasWhiteSpaceAndValidLength, isEmail } from "public/util/functions";
 import { messagesError, messagesSuccess } from "public/util/messages";
+import { db } from "src/firebase";
 export default function ForgotPassword() {
   const authContext = useAuth();
-  const db = getDatabase();
   const [email, setEmail] = useState("");
 
   //validation const
   const [textState, setTextState] = useState("");
   const [isHidden, setIsHidden] = useState(true);
   const [isSuccess, setIsSuccess] = useState(true);
+  const showMethod = useMemo(() => (message, isSuccess, isHidden) => {
+    setTextState(message);
+    setIsSuccess(isSuccess);
+    setIsHidden(isHidden);
+  }, [])
 
   const handleForgot = (email) => {
     // validation
     if (isEmpty(email)) {
-      setTextState(messagesError.E0001("Email"));
-      setIsSuccess(false);
-      setIsHidden(false);
+      showMethod(messagesError.E0001("Email"), false, false);
       return;
     }
 
     if (!isEmail(email)) {
-      setTextState(messagesError.E0003("Email"));
-      setIsSuccess(false);
-      setIsHidden(false);
+      showMethod(messagesError.E0003("Email"), false, false);
       return;
     }
 
     if (hasWhiteSpaceAndValidLength(email)) {
-      setTextState(messagesError.E0005("Email"));
-      setIsSuccess(false);
-      setIsHidden(false);
+      showMethod(messagesError.E0005("Email"), false, false);
       return;
     }
 
@@ -62,30 +59,22 @@ export default function ForgotPassword() {
         const values = Object.values(record);
 
         if (values.length == 0) {
-          setTextState(messagesError.E0009);
-          setIsSuccess(false);
-          setIsHidden(false);
+          showMethod(messagesError.E0009, false, false);
           return;
         } else {
-          setTextState(messagesSuccess.I0003);
-          setIsSuccess(false);
-          setIsHidden(true);
-          authContext.resetPassword(email);
+          showMethod(messagesSuccess.I0003, true, false)
+          //authContext.resetPassword(email);
           return;
         }
       })
     } catch (error) {
-      console.log(messagesError.E1002);
-      setTextState(messagesError.E1002);
-      setIsSuccess(false);
-      setIsHidden(false);
+      showMethod(messagesError.E1002, false, false);
       return;
     }
   };
 
   // show popup
   useEffect(() => {
-    console.log(isHidden)
     if (isHidden == false) {
       isSuccess ? document.getElementById("imgPopup").src = successIcon : document.getElementById("imgPopup").src = failIcon;
       document.getElementById("textState").innerHTML = textState;
