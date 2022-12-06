89fcc0c297254fba6e4114011b630043c66bc8d1
Vũ Phạm Ngọc Tân <19520930@gm.uit.edu.vn>
2022-12-06 10:00:39 +0700

    remove getDatabase

--- public/shared/Header.js
+++ public/shared/Header.js
@@ -10,21 +10,6 @@ import MenuIcon from "public/Icons/menu";
 // Firebase
 import { auth } from "src/firebase";
 
-import {
-  getDatabase,
-  ref,
-  set,
-  child,
-  get,
-  orderByKey,
-  query,
-  orderByChild,
-  equalTo,
-  update,
-  onValue
-} from "firebase/database";
-import { auth } from "src/firebase";
-
 export default function Header() {
   const [showMenu, setShowMenu] = useState(false); //True: Menu will be displayed, False: Menu will be hidden
   const [ userData, setUserData ] = useState({})
@@ -32,43 +17,10 @@ export default function Header() {
   const context = auth.onAuthStateChanged((user) => {//Hiện tại chỉ lưu thông tin của các user đăng nhập/đăng kí thông qua GG
     if(user) setUserData(user);
   });
-
-  const [userData, setUserData] = useState();
-  const db = getDatabase();
   
-
   function handleOpenMenu() {
-    run();
     setShowMenu(!showMenu);
   }
-
-  function fetchDb(user) {
-    // get orderbychild - have conditions
-    const que = query(ref(db, "users"), orderByChild("email"), equalTo(user.email));
-    onValue(que, (snapshot) => {
-        const record = snapshot.val() ?? [];
-        const values = Object.values(record);
-
-        updateUser(values[0]);
-        console.log("header", userData);
-    }
-    );
-}
-
-function updateUser(user) {
-  setUserData(user);
-  console.log(user);
-}
-
-function run() {
-  auth.onAuthStateChanged((user) => {
-    fetchDb(user);
-  })
-}
-
-useEffect(() => {
-  
-}, [])
 
   return (
     <>
