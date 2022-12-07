21a290060b42fba53578e1577bcfaba74d64a33a
Nguyen Anh Tuan <77793919+ng-anhhtuann@users.noreply.github.com>
2022-12-07 08:49:40 +0700

    Create selector and hook for each rule

--- public/redux/selectors/playerSelector.js
+++ public/redux/selectors/playerSelector.js
@@ -1,6 +1,15 @@
 export const selectPlayerReducer = (state) => {
     return state.playerReducer
 }
-export const joinEventSelector = (state) => {
+export const playerEventSelector = (state) => {
     return selectPlayerReducer(state).event
+}
+export const playerUserSelector = (state) => {
+    return selectPlayerReducer(state).user
+}
+export const playerParticipantSelector = (state) => {
+    return selectPlayerReducer(state).participant
+}
+export const playerRewardSelector = (state) => {
+    return selectPlayerReducer(state).reward
 }
