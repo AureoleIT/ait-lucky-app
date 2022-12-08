d22c9a51b14c40cfeffad73aa5d28a6f02761724
phongptt <pttp1512@gmail.com>
2022-12-08 08:45:42 +0700

    Add a common

--- public/util/functions.js
+++ public/util/functions.js
@@ -19,6 +19,6 @@ module.exports = {
       alert("Please Log In to access to this feature!")
       return false//False: chưa đăng nhập / đã đăng xuất
     }
-  }
+  },
   logo: "https://cdn.123job.vn/123job/uploads/2019/09/18/2019_09_18______f334ace51b475d2c562648c2ee9058d3.png",
 };
