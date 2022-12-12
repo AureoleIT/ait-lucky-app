4f26db361bb7cdb351574f302ab3a2bbe7a71c53
ytran30 <a11718trannhuy@gmail.com>
2022-12-12 10:04:53 +0700

    prevent re-render event detail and get reward from firebase to display

--- pages/admin/event/event-detail.js
+++ pages/admin/event/event-detail.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useRouter } from "next/router";
 
@@ -7,71 +7,124 @@ import BgBlueButton from "public/shared/BgBlueButton";
 import SpecialRewardInfo from "public/shared/SpecialRewardInfo";
 import Line from "public/shared/Line";
 
-import testDataAward from "public/util/test";
 import Title from "public/shared/Title";
 
+import { useUserCurrEventHook } from "public/redux/hooks";
+
+import { db } from "src/firebase"
+import {ref, onValue, query, orderByChild, equalTo} from "firebase/database"
 
 function EventDetail() {
-  // router
-  const router = useRouter();
-  
-  // event id (uuid test - apply database later)
-  const eventID = useState(uuidv4().slice(0, 8)); 
-
-  // state
-  const [countdown, setCountdown] = useState(5);
-
-  const optionStyles = {
-    background: "#40BEE5",
-  };
-
-  // logic
-
-  const onSetCoundownTime = () => {
-    let select = document.getElementById("timer").value;
-    setCountdown(select);
-  };
-
-  const handlePrepare = () => {
-    router.push({
-      pathname:"/admin/event/countdown-checkin",
-      query:
-      {
-        countdown
-      }
-    })
-  };
-
-  const handleEditPageNavigation = () => {
-    router.push("/admin/event/edit-event-reward-register");
+    // router
+    const router = useRouter();
     
-  };
+    // eventID
+    const beforeID = useUserCurrEventHook()
+    const eventID = beforeID.slice(0,8)
 
-    return (
-        <div className="flex flex-col justify-evenly items-center h-screen w-screen">
-            {/* test event name */}
-            <Title title={"tiệc cuối năm"}/> 
+    // state
+    const [countdown, setCountdown] = useState(5);
+    const [title, setTitle] = useState("")
+    const [rewards, setRewards] = useState([])
 
+    const optionStyles = {
+        background: "#40BEE5",
+    };
+
+    // logic
+
+    // get name event from firebase
+    const getName = query(ref(db, "event"), orderByChild("eventId"), equalTo(beforeID))
+
+    useEffect(() =>
+    {
+        onValue(getName, (snapshot) =>
+        {
+            const value = snapshot.val()
+            if(value != null)
+            {
+                let eventObject = Object.values(value)
+                setTitle(eventObject[0].title)
+            }
+        })
+    },[])
+
+    // get reward from firebase
+    const reward = query(ref(db, "event_rewards"), orderByChild("eventId"), equalTo(beforeID))
+
+    useEffect(() =>
+    {
+        onValue(reward,(snapshot) =>
+        {
+                const data = snapshot.val()
+                if(data !== null)
+                {
+                    setRewards(Object.values(data))
+                }
+        })
+    },[])
+
+    const onSetCoundownTime = () => {
+        let select = document.getElementById("timer").value;
+        setCountdown(select);
+    };
+
+    const handlePrepare = () => {
+        router.push({
+        pathname:"/admin/event/countdown-checkin",
+        query:
+        {
+            countdown
+        }
+        })
+    };
+
+    const handleEditPageNavigation = () => {
+        router.push("/admin/event/edit-event-reward-register");
+        
+    };
+
+    const renderTitle = useMemo(() =>
+    {
+        return (
+            <Title title={title}/>
+        )
+    },[title])
+
+    const renderEventID = useMemo(() =>
+    {
+        return (
             <div className="flex justify-between w-4/5 max-w-xl text-lg">
                 <h1 className="uppercase py-2 mb-1 font-bold text-[#004599]">mã sự kiện</h1>
                 <h1 className="uppercase py-2 font-bold text-[#004599] tracking-[5px]">{eventID}</h1>
             </div>
-            {/* rewards information */}
-            <h1 className="uppercase text-[#004599] font-bold mb-1 text-[20px]">thông tin giải thưởng</h1>
+        )
+    },[eventID])
 
-            <div className="w-4/5 max-w-xl"> <Line /> </div>
+    const renderH1andLine = useMemo(() =>
+    {
+        return (
+            <>
+                <h1 className="uppercase text-[#004599] font-bold mb-1 text-[20px]">thông tin giải thưởng</h1>
+                <div className="w-4/5 max-w-xl"> <Line /> </div>
+            </>
+        )
+    },[])
 
+    const renderAward = useMemo(() =>
+    {
+        return (
             <div className="flex flex-col overflow-x-hidden overflow-y-auto justify-center items-center w-4/5 max-w-xl h-[350px] my-2">
                 <div className="my-2 w-full h-full flex flex-col max-h-[350px]">
                     {
-                        testDataAward.map((item, index) => {
+                        rewards.map((item, index) => {
                             return (
                                 <div key={index}>
                                     <SpecialRewardInfo
-                                        rewardName={item.nameAward}
-                                        amount={item.amount}
-                                        image={item.image}
-                                        color={item.color}
+                                        rewardName={item.nameReward}
+                                        amount={item.quantity}
+                                        image={item.imgUrl}
+                                        color={"#52FF00"}
                                     />
                                 </div>
                             );
@@ -79,9 +132,19 @@ function EventDetail() {
                     }
                 </div>
             </div>
+        )
+    },[rewards])
 
+    const renderLine2 = useMemo(() =>
+    {
+        return (
             <div className="w-4/5 max-w-xl"> <Line /> </div>
+        )
+    },[])
 
+    const renderCheckinTime = useMemo(() =>
+    {
+        return (
             <div className="flex items-center w-4/5 max-w-xl justify-center mb-2">
                 <div className="flex items-center justify-center h-[40px]">
                     <h1 className="uppercase text-[#004599] font-bold mb-1">thời gian check in</h1>
@@ -100,15 +163,40 @@ function EventDetail() {
                     </select>
                 </div>
             </div>
+        )
+    },[])
 
+    const renderPrepareButton = useMemo(() =>
+    {
+        return (
+            <div className="w-full mr-1 drop-shadow-lg">
+                <BgBlueButton content={"CHUẨN BỊ"} onClick={handlePrepare} />
+            </div>
+        )
+    },[handlePrepare])
+
+    const renderEditPageButton = useMemo(() =>
+    {
+        return (
+            <div className="w-[50px] h-[50px] bg-gradient-to-r from-[#003B93] to-[#00F0FF] rounded-[50px] ml-1 cursor-pointer drop-shadow-lg" onClick={handleEditPageNavigation}>
+                <i className="fas fa-pen text-white flex justify-center items-center w-full h-full"></i>
+            </div>
+        )
+    },[handleEditPageNavigation])
+
+    return (
+        <div className="flex flex-col justify-evenly items-center h-screen w-screen">
+            {/* test event name */}
+            {renderTitle}
+            {renderEventID}
+            {/* rewards information */}
+            {renderH1andLine}
+            {renderAward}
+            {renderLine2}
+            {renderCheckinTime}
             <div className="w-4/5 max-w-xl flex justify-center items-center">
-                <div className="w-full mr-1 drop-shadow-lg">
-                    <BgBlueButton content={"CHUẨN BỊ"} onClick={handlePrepare} />
-                </div>
-
-                <div className="w-[50px] h-[50px] bg-gradient-to-r from-[#003B93] to-[#00F0FF] rounded-[50px] ml-1 cursor-pointer drop-shadow-lg" onClick={handleEditPageNavigation}>
-                    <i className="fas fa-pen text-white flex justify-center items-center w-full h-full"></i>
-                </div>
+                {renderPrepareButton}
+                {renderEditPageButton}  
             </div>
         </div>
     );
