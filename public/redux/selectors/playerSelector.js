670f9d2bf2d4bae6e0e4fd38d018f3264a1cc44a
Nguyen Anh Tuan <77793919+ng-anhhtuann@users.noreply.github.com>
2022-12-14 10:57:28 +0700

    Add update field redux

--- public/redux/selectors/playerSelector.js
+++ public/redux/selectors/playerSelector.js
@@ -1,15 +1,19 @@
 export const selectPlayerReducer = (state) => {
     return state.playerReducer
 }
+// Event object that incognito player joined
 export const playerEventSelector = (state) => {
     return selectPlayerReducer(state).event
 }
+// User object that hosted event above
 export const playerUserSelector = (state) => {
     return selectPlayerReducer(state).user
 }
+// Player object created after successfully joined
 export const playerParticipantSelector = (state) => {
     return selectPlayerReducer(state).participant
 }
+// Reward that this player got
 export const playerRewardSelector = (state) => {
     return selectPlayerReducer(state).reward
 }
