42780dcb7f53703afa10a3ff190ecf5f582a7978
thuyntc <camthuy0212@gmail.com>
2022-12-14 15:50:03 +0700

    Fix bug LineWithText

--- public/shared/Line.js
+++ public/shared/Line.js
@@ -1,32 +1,35 @@
 import React from "react";
-import { LEFT_COLOR, RIGHT_COLOR} from "public/util/colors";
+import { LEFT_GRADIENT, RIGHT_GRADIENT } from "public/util/colors";
 
 export default function Line({
   content, // nội dung được hiển thị trên Line
-  primaryColor = LEFT_COLOR, // màu bên trái
-  secondaryColor = RIGHT_COLOR, // màu bên phải
+  isLineWhite = false, // nếu line màu trắng thì true
   lineWeight = true, // mặc đinh line dày 2px, nếu false thì dày 1px
-  marginY = true // mặc định my-4, nếu false thì mất margin y
+  marginY = true, // mặc định my-4, nếu false thì mất margin y
 }) {
-  const gradientCSS = `bg-gradient-to-r from-[${primaryColor}] to-[${secondaryColor}]`;
-
   return (
-    <div
-      className={`w-full ${
-        lineWeight ? "h-[2px]" : "h-[1px]"
-      } z-10 ${marginY ? "my-4" : ""}  ${gradientCSS} relative flex justify-center`}
-    >
-      <p
-        className={`absolute top-[-50%] transform translate-y-[-50%] font-extrabol bg-white ${
-          !content && "hidden"
-        }`}
+    <div className="w-full justify-center items-center flex mb-5 flex-row">
+      <div
+        className={`flex-grow ${lineWeight ? "h-[2px]" : "h-[1px]"} z-10 ${
+          marginY ? "my-4" : ""
+        }  ${
+          isLineWhite ? "bg-white" : LEFT_GRADIENT
+        } relative flex justify-center`}
+      ></div>
+      <span
+        className={`mx-2 relative bottom-[3px] text-transparent text-xl bg-clip-text font-semibold ${
+          isLineWhite ? "bg-white" : LEFT_GRADIENT
+        } ${content ? "" : "hidden"}`}
       >
-        <span
-          className={`mx-2 text-transparent text-xl bg-clip-text font-semibold ${gradientCSS}`}
-        >
-          {content}
-        </span>
-      </p>
+        {content}
+      </span>
+      <div
+        className={`flex-grow ${lineWeight ? "h-[2px]" : "h-[1px]"} z-10 ${
+          marginY ? "my-4" : ""
+        }  ${
+          isLineWhite ? "bg-white" : RIGHT_GRADIENT
+        } relative flex justify-center`}
+      ></div>
     </div>
   );
 }
