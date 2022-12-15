727261a1b4114823ecc91e868df02579350015c2
thuyntc <camthuy0212@gmail.com>
2022-12-15 09:39:35 +0700

    Fix font-size and margin of Line.js

--- public/shared/Line.js
+++ public/shared/Line.js
@@ -5,30 +5,28 @@ export default function Line({
   content, // nội dung được hiển thị trên Line
   isLineWhite = false, // nếu line màu trắng thì true
   lineWeight = true, // mặc đinh line dày 2px, nếu false thì dày 1px
-  marginY = true, // mặc định my-4, nếu false thì mất margin y
+  marginY = 0, // marginY của line
 }) {
   return (
-    <div className="w-full justify-center items-center flex mb-5 flex-row">
+    <div
+      className={`w-full justify-center items-center my-${marginY} flex flex-row`}
+    >
       <div
-        className={`flex-grow ${lineWeight ? "h-[2px]" : "h-[1px]"} z-10 ${
-          marginY ? "my-4" : ""
-        }  ${
-          isLineWhite ? "bg-white" : LEFT_GRADIENT
-        } relative flex justify-center`}
+        className={`flex-grow z-10 relative flex justify-center
+        ${lineWeight ? "h-[2px]" : "h-[1px]"}  
+        ${isLineWhite ? "bg-white" : LEFT_GRADIENT}`}
       ></div>
       <span
-        className={`mx-2 relative bottom-[3px] text-transparent text-xl bg-clip-text font-semibold ${
-          isLineWhite ? "bg-white" : LEFT_GRADIENT
-        } ${content ? "" : "hidden"}`}
+        className={`mx-2 relative bottom-[3px] text-transparent text-base bg-clip-text font-semibold 
+        ${isLineWhite ? "bg-white" : LEFT_GRADIENT} 
+        ${content ? "" : "hidden"}`}
       >
         {content}
       </span>
       <div
-        className={`flex-grow ${lineWeight ? "h-[2px]" : "h-[1px]"} z-10 ${
-          marginY ? "my-4" : ""
-        }  ${
-          isLineWhite ? "bg-white" : RIGHT_GRADIENT
-        } relative flex justify-center`}
+        className={`flex-grow z-10 relative flex justify-center
+        ${lineWeight ? "h-[2px]" : "h-[1px]"}  
+        ${isLineWhite ? "bg-white" : RIGHT_GRADIENT}`}
       ></div>
     </div>
   );
