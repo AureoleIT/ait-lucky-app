2bfbe00d328c984ef935cbc6b5b2b01d77c155fa
Nguyen Anh Tuan <77793919+ng-anhhtuann@users.noreply.github.com>
2022-12-15 10:04:07 +0700

    Add animation to component

--- public/shared/Logo.js
+++ public/shared/Logo.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function Logo() {
   return (
     <div className="flex flex-col justify-center mb-2 h-1/3 w-auto">
-      <img className=" w-full h-auto mb-2 animate-spin" src={logo}></img>
+      <img className=" w-full h-auto mb-2 motion-safe:animate-bounce" src={logo}></img>
     </div>
   );
 }
