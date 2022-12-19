f340ff97471e117d778fb5575eb403340e969039
vietnh <vieta3pro380@gmail.com>
2022-12-19 16:42:15 +0700

    Setting play sound

--- public/shared/LuckySpinSetting.js
+++ public/shared/LuckySpinSetting.js
@@ -9,13 +9,14 @@ export default function LuckySpinSetting({router}) {
     // const linkPath = router.pathname.split("/");
     // const [isAdmin, setIsAdmin] = useState(false);
     const [musicVol, setMusicVol] = useState(80);
-    const [soundVol, setSoundVol] = useState(80);
+    const [soundVol, setSoundVol] = useState(0);
     const [muteMusic, setMuteMusic] = useState(true);
     const [muteSound, setMuteSound] = useState(true);
 
-    // useEffect(() => {
-    //     if (linkPath[1] === "admin") setIsAdmin(true);
-    // }, [])
+    useEffect(() => {
+        document.getElementById("gameSound").volume = soundVol/100;
+        document.getElementById("gameMusic").volume = musicVol/100;
+    }, [])
 
     useEffect(() => {
         if (!muteMusic && musicVol > 0) {
@@ -59,24 +60,13 @@ export default function LuckySpinSetting({router}) {
         return size;
     }
 
-    // const exitEvent = () => {
-    //     router.push('/');
-    // }
-
-    // const finishEvent = () => {
-    //     update(ref(db, 'event/' + router.query["eventId"]),
-    //     {
-    //         status: 4
-    //     });
-    // }
-
     return (
         <div className="flex flex-col px-3 gap-4">
-            <audio loop className="hidden" id="gameMusic">
+            <audio loop className="hidden" id="gameMusic" volume={8}>
                 <source src="https://joeybabcock.me/blog/wp-content/uploads/2019/05/lobby-classic-game-halloween.mp3" type="audio/mpeg"></source>
             </audio>
-            <audio loop className="hidden" id="gameSound">
-                <source src="https://joeybabcock.me/blog/wp-content/uploads/2019/05/lobby-classic-game-halloween.mp3" type="audio/mpeg"></source>
+            <audio className="hidden" id="gameSound" volume={0}>
+                <source src="https://wheelofnames.com/sounds/ding.mp3" type="audio/mpeg"></source>
             </audio>
             <div className="flex gap-2">
                 <label htmlFor="gameSoundVolume" className={!muteMusic?"bg-[#40BEE5] w-10 h-10 p-2 rounded-full":"bg-[#888] w-10 h-10 p-2 rounded-full"}
@@ -109,7 +99,7 @@ export default function LuckySpinSetting({router}) {
                             <path d="M13.5 4.06c0-1.336-1.616-2.005-2.56-1.06l-4.5 4.5H4.508c-1.141 0-2.318.664-2.66 1.905A9.76 9.76 0 001.5 12c0 .898.121 1.768.35 2.595.341 1.24 1.518 1.905 2.659 1.905h1.93l4.5 4.5c.945.945 2.561.276 2.561-1.06V4.06zM17.78 9.22a.75.75 0 10-1.06 1.06L18.44 12l-1.72 1.72a.75.75 0 001.06 1.06l1.72-1.72 1.72 1.72a.75.75 0 101.06-1.06L20.56 12l1.72-1.72a.75.75 0 00-1.06-1.06l-1.72 1.72-1.72-1.72z" />                        }
                     </svg>
                 </label>
-                <input type="range" id="gameSoundVolume" name="gameSoundVolume" min="0" max="10" defaultValue="8" className="grow muteInput"
+                <input type="range" id="gameSoundVolume" name="gameSoundVolume" min="0" max="10" defaultValue="0" className="grow muteInput"
                     style={{"--background-size": soundVol + "%"}}
                     onInput={() => setBackgroundSize("gameSound")}></input>
             </div>
