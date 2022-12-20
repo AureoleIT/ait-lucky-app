d7f162a43436784da29b172182f0718dd28da931
phongptt <pttp1512@gmail.com>
2022-12-20 08:37:29 +0700

    add margin prop for Title

--- public/shared/Title.js
+++ public/shared/Title.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-export default function Title({title, fontSize = "36", fontWeight = "[900]"}) {
-// font size (px) (default: 36px)
-
-    return (
-        <h1 className={`font-${fontWeight} uppercase text-[#004599] text-[${fontSize}px] text-center mb-5`}>{title}</h1>
-    )
+export default function Title({
+  title,
+  fontSize = "text-[36px]",
+  fontWeight = "font-[900]",
+  margin = "mb-5"
+}) {
+  return (
+    <h1
+      className={`${fontWeight} uppercase text-[#004599] ${fontSize} text-center ${margin}`}
+    >
+      {title}
+    </h1>
+  );
 }
