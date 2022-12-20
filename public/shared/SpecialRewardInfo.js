f6669a162d14e80b5ac30a2274093cb0a417c66b
ytran30 <a11718trannhuy@gmail.com>
2022-12-20 15:48:12 +0700

    use new component - SpecialRewardInfo

--- public/shared/SpecialRewardInfo.js
+++ public/shared/SpecialRewardInfo.js
@@ -11,11 +11,11 @@ function SpecialRewardInfo ({ rewardName, amount, image, color }) {
     return (
         <div className="my-1 w-[100%]">
             <div className="w-[100%] min-h-fit flex justify-between items-center px-3 text-[16px] rounded-[50px] mb-2 drop-shadow-xl" style={headerColor}>
-                <h1 className="uppercase font-bold w-[70%]">{rewardName}</h1>
-                <h1 className="w-[30%] text-right">Số lượng : {amount}</h1>
+                <h1 className="font-[700] uppercase text-[#004599] w-[65%]">{rewardName}</h1>
+                <h1 className="w-[35%] text-right font-[700] text-[#004599]">Số lượng : {amount}</h1>
             </div>
 
-            <div className="w-[100%] h-[120px] flex overflow-x-auto overflow-y-hidden" style={divBorder}>
+            <div className="w-[100%] h-[120px] flex items-center overflow-x-auto overflow-y-hidden" style={divBorder}>
                 <div className="w-full h-[100px] flex">
                     {
                         Array.isArray(image) ? (
