409eb1754743dd093b89a86a8f9011a8b8118279
thuyntc <camthuy0212@gmail.com>
2022-12-21 09:24:19 +0700

    use router instead of <a>

--- public/shared/button/EventButton.js
+++ public/shared/button/EventButton.js
@@ -1,3 +1,4 @@
+import router from "next/router";
 import React from "react";
 
 export default function EventButton({
@@ -8,23 +9,40 @@ export default function EventButton({
   onclick,
 }) {
   return (
-    <a href={`${setLink(status)}`} className="flex flex-col w-full">
+    <div onClick={handleClick} className="flex flex-col w-full">
       <button
         className={`rounded-[5px] mx-2 ${setColor(status, id, userJoined)}`}
         onClick={onclick}
       >
         <div className="flex justify-between items-center ml-4 mr-2 text-white h-10 font-[Nunito Sans]">
           <div className="justify-center items-center text-left font-bold text-sm uppercase truncate w-1/2">
-            {title}
+            {title ? <> {title}</> : <> Title not available</>}
           </div>
           <div className="text-xs flex flex-col flex-1 text-right ml-10 items-right truncate break-words">
-           { id === -1 ? <></> : <> {id ? <div className="truncate">
-                {id}
-              </div>
-            : (
-              <>id not available</>
-            )}</>}
-            {userJoined >=0 ? <div>{userJoined} người tham gia </div> : <> { userJoined === -1 ? <></> : <div>players not available</div>} </>}
+            {id === -1 ? (
+              <></>
+            ) : (
+              <>
+                {" "}
+                {id ? (
+                  <div className="truncate">{id}</div>
+                ) : (
+                  <>Id not available</>
+                )}
+              </>
+            )}
+            {userJoined >= 0 ? (
+              <div>{userJoined} người tham gia </div>
+            ) : (
+              <>
+                {" "}
+                {userJoined === -1 ? (
+                  <></>
+                ) : (
+                  <div>Players not available</div>
+                )}{" "}
+              </>
+            )}
           </div>
           <div className="ml-2">
             <svg
@@ -44,39 +62,51 @@ export default function EventButton({
           </div>
         </div>
       </button>
-    </a>
+    </div>
   );
 }
 
 function setColor(status, id, userJoined) {
-  if (id===-1 || userJoined === -1) 
-  {return "bg-[#40BEE5]"}
-  else
-  {switch (status) {
-    case 1:
-      return "bg-[#58BC66]";
-    case 2:
-      return "bg-[#D2D440]";
-    case 3:
-      return "bg-[#DDAF0C]";
-    case 4:
-      return "bg-[#FF6262]";
-    default:
-      return "bg-[#40BEE5]";
-  }}
-}
-
-function setLink(status) {
-  switch (status) {
-    case 1:
-      return "/admin/event/event-detail";
-    case 2:
-      return "/admin/event/countdown-checkin";
-    case 3:
-      return "/admin/luckyspin";
-    case 4:
-      return "/event/event-result";
-    default:
-      return "/admin/event/event-detail";
+  if (id === -1 || userJoined === -1) {
+    return "bg-[#40BEE5]";
+  } else {
+    switch (status) {
+      case 1:
+        return "bg-[#58BC66]";
+      case 2:
+        return "bg-[#D2D440]";
+      case 3:
+        return "bg-[#DDAF0C]";
+      case 4:
+        return "bg-[#FF6262]";
+      default:
+        return "bg-[#40BEE5]";
+    }
   }
 }
+
+function handleClick(status) {
+  const statusEvent = 2;
+  switch (status) {
+    case 1:
+      return router.push({
+        pathname: "/admin/event/event-detail",
+        query: { statusEvent },
+      });
+    case 2:
+      return router.push({
+        pathname: "/admin/event/countdown-checkin",
+        query: { statusEvent },
+      });
+    case 3:
+      return router.push("/admin/luckyspin");
+    case 4:
+      return router.push("/event/event-result");
+    default:
+      return router.push({
+        pathname: "/admin/event/event-detail",
+        query: { statusEvent },
+      });
+  }
+}
+// href={`${setLink(status)}`}
