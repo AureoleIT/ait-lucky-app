bc03a89f53b313ff46338c7e3423e1b2084de37a
dungdung7301 <81977385+dungdung7301@users.noreply.github.com>
2022-12-21 17:03:02 +0700

    Reduce import, add condition to query and fix FE

--- pages/admin/dashboard-admin.js
+++ pages/admin/dashboard-admin.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect, useMemo } from "react";
 // import firebase
 import { db } from "src/firebase";
-import { ref, orderByChild, equalTo, query, onValue } from "firebase/database";
+import {
+  ref,
+  orderByChild,
+  equalTo,
+  query,
+  onValue,
+  limitToLast,
+} from "firebase/database";
 
 // components
-import Header from "public/shared/Header";
+import { Header, Input, Line, Button } from "public/shared";
 import EventButton from "public/shared/button/EventButton";
-import Input from "public/shared/Input";
 import { LEFT_COLOR, RIGHT_COLOR } from "public/util/colors";
-import Line from "public/shared/Line";
+// import Input from "public/shared/Input";
+// import Line from "public/shared/Line";
+// import Button from "public/shared/Button";
 
 //import gif
 import nyancat from "public/img/nyancat.gif";
 import { useUserPackageHook } from "public/redux/hooks";
 import router from "next/router";
-import Button from "public/shared/Button";
 
 export default function Dashboard() {
   const [arrStatus, setArrStatus] = useState([]);
@@ -26,6 +33,7 @@ export default function Dashboard() {
   const queID = query(
     ref(db, "event"),
     orderByChild("createBy"),
+    limitToLast(4),
     equalTo(String(currentUser.userId))
   );
   const checkAuth = () => {
@@ -60,60 +68,84 @@ export default function Dashboard() {
     onValue(queStatus, (snapshot) => {
       setArrStatus([]);
       const data = snapshot.val();
+      const values = Object.values(data);
       if (data != null) {
-        setArrStatus(Object.values(data));
+        values.forEach((value) => {
+          if (value.delFlag === false) setArrStatus((prev) => [...prev, value]);
+          // if (value.delFlag === false) setArrStatus(values)
+        });
       }
     });
-  }, []);
+  }, [2]);
 
   useEffect(() => {
     onValue(queID, (snapshot) => {
       setArrID([]);
       const data = snapshot.val();
-      if (data != null) {
-        setArrID(Object.values(data));
+      const values = Object.values(data);
+        if (data != null) {
+          values.forEach((value) => {
+          if (value.delFlag === false )
+          setArrID((prev) => [...prev, value]);
+          });
       }
-    });
+     });
   }, [String(currentUser.userId)]);
 
   const renderHeader = useMemo(() => {
     return <Header />;
   }, []);
 
+  const renderWelcome = useMemo(() => {
+    return (
+      <div className="flex flex-col pb-4 pt-2">
+        <div className="flex flex-col">
+          <div className="flex flex-col flex-1">
+            <p className="font-bold text-sm text-[#656565] mt-2">
+              {"Chào mừng đến với AIT Lucky App,"}
+            </p>
+          </div>
+
+          <div className="flex justify-between items-end w-full">
+            <p className="text-sm text-[#656565] mb-2">
+              {"hãy bắt đầu chơi các sự kiện ngay nào!"}
+            </p>
+            <img
+              src={nyancat}
+              className="w-1/5 min-h-min "
+              alt="must be a nyancat gif"
+            ></img>
+          </div>
+        </div>
+        <div className="w-full mb-2">
+          <Line marginY={false} />
+        </div>
+      </div>
+    );
+  }, []);
   const renderJoinEvent = useMemo(() => {
     return (
       <Input content={"Tham gia sự kiện"}>
         <div className="flex flex-col pb-4 pt-2">
-          <div className="flex flex-col">
-            <div className="flex flex-col flex-1">
-              <p className="font-bold text-sm text-[#656565] mt-2">
-                {"Chào mừng đến với AIT Lucky App,"}
-              </p>
-            </div>
-
-            <div className="flex justify-between items-end w-full">
-              <p className="text-sm text-[#656565] mb-2">
-                {"hãy bắt đầu chơi các sự kiện ngay nào!"}
-              </p>
-              <img src={nyancat} className="md:w-1/5 md:h-min w-[30px] h-[20.7px]" alt="must be a nyancat gif"></img>
-            </div>
-          </div>
-          <div className="w-full mb-2">
-            <Line marginY={false} />
-          </div>
-          <p className=" font-bold text-sm text-[#656565]">
+          <p className=" f text-sm text-[#656565] my-2">
             {"Tham gia vào các sự kiện được tổ chức bằng mã pin."}
           </p>
           <a href="/">
             <Button
+              margin={"my-0"}
               content={"CHƠI VỚI MÃ PIN!"}
               primaryColor={LEFT_COLOR}
               secondaryColor={RIGHT_COLOR}
             />
           </a>
-          <p className="font-bold text-sm text-[#000000] pb-2">
-            {"Các sự kiện đang diễn ra"}
-          </p>
+        </div>
+      </Input>
+    );
+  }, []);
+  const renderShowCurrentEvent = useMemo(() => {
+    return (
+      <Input content={"Các sự kiện đang diễn ra"} isTextGradient={true}>
+        <div className="flex flex-col py-4">
           <div className="w-full flex flex-col gap-y-[7px] overflow-auto max-h-[188px] scrollbar-hide">
             {arrStatus.length === 0 ? (
               <div className="w-full flex items-center text-center justify-center text-sm text-[#000000]">
@@ -123,7 +155,8 @@ export default function Dashboard() {
             ) : (
               arrStatus.map((item, index) => (
                 <div key={index} className="flex flex-col">
-                  <EventButton className="bg-[#40BEE5]"
+                  <EventButton
+                    className="bg-[#40BEE5]"
                     title={item.title}
                     userJoined={item.userJoined}
                     status={item.status}
@@ -136,7 +169,6 @@ export default function Dashboard() {
       </Input>
     );
   }, [arrStatus]);
-
   const renderCreateEvent = useMemo(() => {
     return (
       <Input content={"Tạo sự kiện"} isTextGradient={true}>
@@ -158,10 +190,10 @@ export default function Dashboard() {
     );
   }, []);
 
-  const renderShowEvent = useMemo(() => {
+  const renderShowCreateEvent = useMemo(() => {
     return (
       <Input content={"Danh sách sự kiện"} isTextGradient={true}>
-        <p className="font-bold text-sm text-[#656565] mt-3 mb-2">
+        <p className=" text-sm text-[#656565] mt-5 mb-2">
           {"Hiển thị các sự kiện của tôi đã tạo"}
         </p>
         <div className="flex flex-col gap-y-[7px]">
@@ -171,7 +203,7 @@ export default function Dashboard() {
               {"Không có dữ liệu"}
             </div>
           ) : (
-            arrID.slice(0, 4).map((item, index) => (
+            arrID.map((item, index) => (
               <div key={index} className="flex flex-col">
                 <EventButton title={item.title} id={item.eventId} />
               </div>
@@ -179,7 +211,7 @@ export default function Dashboard() {
           )}
           <a href="event-list">
             <Button
-              content={"TẤT CẢ SỰ KIỆN"}
+              content={"Tất cả sự kiện"}
               primaryColor={LEFT_COLOR}
               secondaryColor={RIGHT_COLOR}
             />
@@ -197,12 +229,16 @@ export default function Dashboard() {
         <div>
           {renderHeader}
           <section className="h-full max-w-xl w-4/5 mx-auto flex flex-col justify-center items-center pt-2">
+            {/* {welcome to AIT App} */}
+            {renderWelcome}
             {/* participate in event */}
             {renderJoinEvent}
             {/* create a event */}
+            {renderShowCurrentEvent}
+            {/*  */}
             {renderCreateEvent}
             {/* show events */}
-            {renderShowEvent}
+            {renderShowCreateEvent}
           </section>
         </div>
       )}
