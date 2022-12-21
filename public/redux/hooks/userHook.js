77473c8d8d4e90d0128acb54f67c0e35088181a1
Nguyen Anh Tuan <77793919+ng-anhhtuann@users.noreply.github.com>
2022-12-21 10:01:00 +0700

    Delete unused fields

--- public/redux/hooks/userHook.js
+++ public/redux/hooks/userHook.js
@@ -2,28 +2,13 @@ import { useSelector } from "react-redux";
 import {
     userCurrentEventHostingSelector,
     userCurrentEventPlayingSelector,
-    userEventSelector,
     userPackageSelector,
-    userParticipantSelector,
-    userRewardSelector,
     userEventCreatingSelector,
     userRewardCreatingSelector
 } from "../selectors";
-// All event from user
-export const useUserEventHook = () => {
-    return useSelector(userEventSelector);
-}
 // Current user object 
 export const useUserPackageHook = () => {
     return useSelector(userPackageSelector);
-}
-// All participant from events this user had or hosted
-export const useUserParticipantHook = () => {
-    return useSelector(userParticipantSelector);
-}
-// All reward from events this user had or hosted
-export const useUserRewardHook = () => {
-    return useSelector(userRewardSelector);
 }
 // Event that this user host
 export const useUserCurrEventHostingHook = () => {
