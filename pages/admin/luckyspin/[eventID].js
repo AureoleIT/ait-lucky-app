bd0890b65107b3e2f21c45fbd717227ec53ca89a
vietnh <vieta3pro380@gmail.com>
2022-12-28 13:40:32 +0700

    set offline for admin

--- pages/admin/luckyspin/[eventID].js
+++ pages/admin/luckyspin/[eventID].js
@@ -95,14 +95,12 @@ export default function LuckySpinAdmin() {
             if (dataset[0].exists()) {
                 const rawData = dataset[0].val();
                 const dataEventParticipant = Object.values(rawData);
-                // dataEventParticipant.forEach((val, idx) => {
-                //     val.ID = Object.keys(rawData)[idx];
-                //     get(child(ref(db), "users/" + val.createBy)).then((snapshot) => {
-                //         if (snapshot.exists()) {
-                //             val['pic'] = snapshot.val().pic;
-                //         }
-                //     })
-                // })
+                data.forEach((val, idx) => {
+                    if (val.status !== 0)
+                        update(ref(db, 'event_participants/' + val.participantId), {
+                            status: 1
+                        });
+                })
                 const online = dataEventParticipant.filter(val => val.status === 1).length;
                 const filted = dataEventParticipant.filter(val => (val.idReward === "" && val.status === 1));
                 setPlayerList(rawData);
@@ -184,14 +182,12 @@ export default function LuckySpinAdmin() {
             if (snapshot.exists()) {
                 const rawData = snapshot.val();
                 const data = Object.values(rawData);
-                // data.forEach((val, idx) => {
-                //     val.ID = Object.keys(rawData)[idx];
-                //     get(child(ref(db), "users/" + val.createBy)).then((snapshot) => {
-                //         if (snapshot.exists()) {
-                //             val['pic'] = snapshot.val().pic;
-                //         }
-                //     })
-                // })
+                data.forEach((val, idx) => {
+                    if (val.status !== 0)
+                        update(ref(db, 'event_participants/' + val.participantId), {
+                            status: 1
+                        });
+                })
                 const online = data.filter(val => val.status === 1).length;
                 const filted = data.filter(val => (val.idReward === "" && val.status === 1));
                 setPlayerList(rawData);
