888b40f6c49cc0817aa34a8ff8294ee7e7d9693a
thuyntc <camthuy0212@gmail.com>
2023-01-05 09:02:53 +0700

    Fix router

--- public/trans/hooks/Trans.js
+++ public/trans/hooks/Trans.js
@@ -1,10 +1,10 @@
-import { useRouter } from "next/router";
+import router, { useRouter } from "next/router";
 import vi from "../lang/vi";
 import en from "../lang/en";
 
 const Trans = () => {
-  const { locale } = useRouter() || {};
-
+  const { locale } = router;
+  console.log(locale);
   const trans = locale === "en" ? en : vi;
 
   return trans;
